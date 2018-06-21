03864fee6f2c3884ad345a8ce4e25893bc42adb2
Zachariah Etienne <zachetie@gmail.com>
2018-06-20 23:59:46 -0400

    Reference metric tutorial

--- reference_metric__hatted_quantities.py
+++ reference_metric__hatted_quantities.py
@@ -10,18 +10,17 @@ import reference_metric as rfm
 thismodule = __name__
 # par.initialize_param(par.glb_param("char", thismodule, "CoordSystem", "Cartesian"))
 
-def reference_metric__hatted_quantities():
-    rfm.reference_metric()
+def reference_metric__hatted_quantities(call_reference_metric=False):
+    if call_reference_metric:
+        rfm.reference_metric()
     
     # Step 0: Set dimension DIM
     DIM = par.parval_from_str("grid::DIM")
 
+    global ReU,ReDD,ghatDD
     ReU    = ixp.zerorank1()
-    global ReU
     ReDD   = ixp.zerorank2()
-    global ReDD
     ghatDD = ixp.zerorank2()
-    global ghatDD
 
     # Step 1: Compute ghatDD (reference metric), as well as 
     #         rescaling vector ReU & rescaling matrix ReDD
@@ -49,16 +48,17 @@ def reference_metric__hatted_quantities():
                 exit(1)
 
     # Step 2: Compute det(ghat) and its 1st & 2nd derivatives
+    global detgammahatdD,detgammahatdDD
     detgammahatdD  = ixp.zerorank1(DIM)
     detgammahatdDD = ixp.zerorank2(DIM)
     for i in range(DIM):
         detgammahatdD[i] = (sp.diff(detgammahat, rfm.xx[i]))
         for j in range(DIM):
             detgammahatdDD[i][j] = sp.diff(detgammahatdD[i], rfm.xx[j])
-    global detgammahatdD,detgammahatdDD
 
     # Step 3a: Compute 1st & 2nd derivatives of rescaling vector.
     #          (E.g., needed in BSSN for betaUdDD computation)
+    global ReUdD,ReUdDD
     ReUdD  = ixp.zerorank2(DIM)
     ReUdDD = ixp.zerorank3(DIM)
     for i in range(DIM):
@@ -66,9 +66,9 @@ def reference_metric__hatted_quantities():
             ReUdD[i][j] = sp.diff(ReU[i], rfm.xx[j])
             for k in range(DIM):
                 ReUdDD[i][j][k] = sp.diff(ReUdD[i][j], rfm.xx[k])
-    global ReUdD,ReUdDD
 
     # Step 3b: Compute 1st & 2nd derivatives of rescaling matrix.
+    global ReDDdD,ReDDdDD
     ReDDdD = ixp.zerorank3(DIM)
     ReDDdDD = ixp.zerorank4(DIM)
     for i in range(DIM):
@@ -78,9 +78,9 @@ def reference_metric__hatted_quantities():
                 for l in range(DIM):
                     # Simplifying this doesn't appear to help overall NRPy run time.
                     ReDDdDD[i][j][k][l] = sp.diff(ReDDdD[i][j][k],rfm.xx[l])
-    global ReDDdD,ReDDdDD
 
     # Step 3c: Compute 1st & 2nd derivatives of reference metric.
+    global ghatDDdD,ghatDDdDD
     ghatDDdD = ixp.zerorank3(DIM)
     ghatDDdDD = ixp.zerorank4(DIM)
     for i in range(DIM):
@@ -89,9 +89,9 @@ def reference_metric__hatted_quantities():
                 ghatDDdD[i][j][k] = sp.simplify(sp.diff(ghatDD[i][j],rfm.xx[k])) # FIXME: BAD: MUST BE SIMPLIFIED OR ANSWER IS INCORRECT! Must be some bug in sympy...
                 for l in range(DIM):
                     ghatDDdDD[i][j][k][l] = (sp.diff(ghatDDdD[i][j][k],rfm.xx[l]))
-    global ghatDDdD,ghatDDdDD
 
     # Step 4a: Compute Christoffel symbols of reference metric.
+    global GammahatUDD
     GammahatUDD = ixp.zerorank3(DIM)
     for i in range(DIM):
         for k in range(DIM):
@@ -99,13 +99,12 @@ def reference_metric__hatted_quantities():
                 for m in range(DIM):
                     GammahatUDD[i][k][l] += (sp.Rational(1,2))*ghatUU[i][m]*\
                                             (ghatDDdD[m][k][l] + ghatDDdD[m][l][k] - ghatDDdD[k][l][m])
-    global GammahatUDD
 
     # Step 4b: Compute derivs of Christoffel symbols of reference metric.
+    global GammahatUDDdD
     GammahatUDDdD = ixp.zerorank4(DIM)
     for i in range(DIM):
         for j in range(DIM):
             for k in range(DIM):
                 for l in range(DIM):
                     GammahatUDDdD[i][j][k][l] = (sp.diff(GammahatUDD[i][j][k],rfm.xx[l]))
-    global GammahatUDDdD
