12bb3635d41ae9af060eb853130b71557f31f4eb
Zachariah Etienne <zachetie@gmail.com>
2018-07-17 12:22:40 -0400

    Start work on .py module files

---
+++ ScalarWaveCurvilinear/ScalarWaveCurvilinear.py
@@ -0,0 +1,108 @@
+# Generating C code for the right-hand-side 
+#  of the scalar wave equation, in 
+#  ***curvilinear*** coordinates, using a 
+#  reference-metric formalism
+#
+# Author: Zachariah B. Etienne
+#         zachetie **at** gmail **dot* com
+# 
+# License: BSD 2-Clause
+
+# COMPLETE DOCUMENTATION (JUPYTER NOTEBOOKS):
+# START PAGE (start here!):  ../NRPy+_Tutorial.ipynb
+# THIS MODULE: ../Tutorial-ScalarwaveCurvilinear.ipynb
+
+# Recall from the documentation, the scalar wave
+#   equation in curvilinear coordinates can be
+#   written as
+# \hat{g}^{mu nu} \partial_{mu} \partial_{nu} u - \hat{\Gamma}^{tau} \partial_{tau} u ,
+# where \hat{\Gamma}^{tau} is the *contracted* Christoffel symbol.
+#
+# For reference metrics supported by NRPy+,
+#   \hat{g}_{t nu} = -\delta_{t nu},
+#   where \delta_{t nu} is the Kronecker delta, we have
+# \partial_t^2 u = \hat{g}^{i j} \partial_{i} \partial_{j} u - \hat{\Gamma}^i \partial_i u ,
+#   where Latin indices imply the spatial components *only*.
+#
+# We break up the above equation into two first-order-in-time PDEs,
+#   defining v = \partial_t u:
+#
+# \partial_t u = v
+# \partial_t v = \hat{g}^{i j} \partial_{i} \partial_{j} u - \hat{\Gamma}^i \partial_i u
+
+# Step P1: Import needed NRPy+ core modules:
+from outputC import *
+import NRPy_param_funcs as par
+import indexedexp as ixp
+import reference_metric as rfm
+import grid as gri
+import finite_difference as fin
+
+# Step P2: Define the C parameter wavespeed. The `wavespeed`
+#          variable is a proper SymPy variable, so it can be
+#          used in below expressions. In the C code, it acts
+#          just like a usual parameter, whose value is
+#          specified in the parameter file.
+# The name of this module ("ScalarWaveCurvlinear") is given by __name__:
+thismodule = __name__
+wavespeed = par.Cparameters("REAL", thismodule, "wavespeed")
+
+def ScalarWaveCurvilinear():
+    # Step 1: Set spatial dimension.
+    #         With reference metrics, this must be
+    #         set to 3 or fewer.
+    DIM = 3 # Three spatial dimensions
+    par.set_parval_from_str("grid::DIM",DIM)
+
+    # Step 2: Set up the reference metric and
+    #         quantities derived from the
+    #         reference metric.
+    rfm.reference_metric()
+
+    # Step 3: Compute the contracted Christoffel symbols:
+    contractedGammahatU = ixp.zerorank1()
+    for k in range(DIM):
+        for i in range(DIM):
+            for j in range(DIM):
+                contractedGammahatU[k] += rfm.ghatUU[i][j] * rfm.GammahatUDD[k][i][j]
+
+    # Step 4: Register gridfunctions that are needed as input.
+    uu, vv = gri.register_gridfunctions("EVOL",["uu","vv"])
+
+    # Step 5a: Declare the rank-1 indexed expression \partial_{i} u,
+    #          Derivative variables like these must have an underscore
+    #          in them, so the finite difference module can parse the
+    #          variable name properly.
+    uu_dD = ixp.declarerank1("uu_dD")
+
+    # Step 5b: Declare the rank-2 indexed expression \partial_{ij} u,
+    #          which is symmetric about interchange of indices i and j
+    #          Derivative variables like these must have an underscore
+    #          in them, so the finite difference module can parse the
+    #          variable name properly.
+    uu_dDD = ixp.declarerank2("uu_dDD","sym12")
+
+    # Step 6: Define right-hand sides for the evolution.
+    # Step 6a: uu_rhs = vv:
+    uu_rhs = vv
+    # Step 6b: The right-hand side of the \partial_t v equation
+    #          is given by:
+    #          \hat{g}^{ij} \partial_i \partial_j u - \hat{\Gamma}^i \partial_i u.
+    #          ^^^^^^^^^^^^ PART 1 ^^^^^^^^^^^^^^^^ ^^^^^^^^^^ PART 2 ^^^^^^^^^^^
+    vv_rhs = 0
+    for i in range(DIM):
+        # PART 2:
+        vv_rhs -= contractedGammahatU[i]*uu_dD[i]
+        for j in range(DIM):
+            # PART 1:
+            vv_rhs += rfm.ghatUU[i][j]*uu_dDD[i][j]
+
+    vv_rhs *= wavespeed*wavespeed
+
+    vv_rhs = sp.simplify(vv_rhs)
+
+    # Step 7: Generate C code for scalarwave evolution equations,
+    #         print output to the screen (standard out, or stdout).
+    fin.FD_outputC("stdout",
+                   [lhrh(lhs=gri.gfaccess("rhs_gfs","uu"),rhs=uu_rhs),
+                    lhrh(lhs=gri.gfaccess("rhs_gfs","vv"),rhs=vv_rhs)])
