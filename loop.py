6734fa55c9853c0a8e45906ce50d5f929244ce71
Zachariah Etienne <zachetie@gmail.com>
2018-07-20 13:07:42 -0400

    continue work on loop code

--- loop.py
+++ loop.py
@@ -1,19 +1,27 @@
-def loop1D(idxvar="i0",loweridx="0",upperidx="Nx0",increment="1",OpenMPpragma="#pragma omp parallel for",prefix=""):
-    str = OpenMPpragma + "\n"
-    str += prefix+"for(int "+idxvar+"="+loweridx+"; "+idxvar+"<"+upperidx+"; "+idxvar+"+="+increment+") {"
-    return str
+def loop1D(idxvar="i0",lower="0",upper="Nx0",incr="1",OpenMPpragma="#pragma omp parallel for",prefix=""):
+    OMPheader = ""
+    if OpenMPpragma != "":
+        OMPheader = OpenMPpragma + "\n"
+    incrstr = "++"
+    if incr != "1":
+        incrstr = "+="+incr
+    loopbody = prefix+"for(int "+idxvar+"="+lower+"; "+idxvar+"<"+upper+"; "+idxvar+incrstr+")"
+    return OMPheader+loopbody+" {\n", prefix+"} // END LOOP: "+loopbody.replace(prefix,"")+"\n"
 
-def loop(idxvar=["i0","i1"],
-         loweridx=["0","0"],
-         upperidx=["Nx0","Nx1"],
-         increment=["1","1"],
-         OpenMPpragma=["","#pragma omp parallel for"]):
+def loop(idxvar,lower,upper,incr,OpenMPpragma):
     prefix = ""
-    str = ""
+    header = ""
+    footerarray = []
     for i in range(len(idxvar)):
-        str += loop1D(idxvar[i],loweridx[i],upperidx[i],increment[i],OpenMPpragma[i],prefix) + "\n"
+        headerstr,footerstr = loop1D(idxvar[i],lower[i],upper[i],incr[i],OpenMPpragma[i],prefix)
+        header += headerstr
+        footerarray.append(footerstr)
         prefix += "    "
-    return str
+    footer = ""
+    for i in range(len(idxvar)-1,-1,-1):
+        footer += footerarray[i]
+    return header,footer
 
-loopstr = loop()
-print(loopstr)
+loopheader,loopfooter = loop(idxvar=["i0","i1"],lower=["0","0"],upper=["Nx0","Nx1"],incr=["1","1"],
+                             OpenMPpragma=["", "#pragma omp parallel for"])
+print(loopheader+loopfooter)
