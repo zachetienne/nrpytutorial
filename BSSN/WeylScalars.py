78c3cbfea45d327da87c257f82f859dffd8c3923
Zachariah Etienne <zachetie@gmail.com>
2018-07-24 20:28:44 -0400

    Continue implementing best-practices for WeylScalars.py

--- BSSN/WeylScalars.py
+++ BSSN/WeylScalars.py
@@ -1,4 +1,6 @@
-# This module computes the the Weyl scalars based on Baker, Campanelli, and Lousto. PRD 65, 044001 (2002).
+# This module computes the the Weyl scalars based on
+# Baker, Campanelli, and Lousto. PRD 65, 044001 (2002);
+#  https://arxiv.org/abs/gr-qc/0104063
 
 # Step 1: import all needed modules from NRPy+:
 import NRPy_param_funcs as par
@@ -14,7 +16,7 @@ import sympy as sp
 # Step 1: Initialize WeylScalar parameters
 thismodule = __name__
 # Use proper names for Tetrad Choices. If no name given (hunt the literature), then use the literature reference as the name.
-TetradChoice = par.initialize_param(par.glb_param("char", thismodule, "TetradChoice", "SOME NAME"))
+TetradChoice = par.initialize_param(par.glb_param("char", thismodule, "TetradChoice", "Approx_QuasiKinnersley"))
 # Why are these needed?
 # xorig = par.initialize_param(par.glb_param("REAL", thismodule, "xorig", "0.0"))
 # yorig = par.initialize_param(par.glb_param("REAL", thismodule, "yorig", "0.0"))
@@ -44,21 +46,20 @@ def WeylScalars():
     DIM = 3
     par.set_parval_from_str("grid::DIM",DIM)
 
-    x,y,z = gri.register_gridfunctions("AUX",["x","y","z"])
+    x = rfm.xxCart[0]
+    y = rfm.xxCart[1]
+    z = rfm.xxCart[2]
 
-    if TetradChoice is 0:
+    if TetradChoice == "Approx_QuasiKinnersley":
+        # Eqs 5.6 in https://arxiv.org/pdf/gr-qc/0104063.pdf
         xmoved = x - xorig  # Make sure I'm handling coordinates correctly
         ymoved = y - yorig
         zmoved = z - zorig
-        vaU = ixp.declarerank1("vaU")
-        vbU = ixp.declarerank1("vbU")
-        vcU = ixp.declarerank1("vcU")
-        waU = ixp.declarerank1("waU")
-        wbU = ixp.declarerank1("wbU")
-        wcU = ixp.declarerank1("wcU")
-        eaU = ixp.declarerank1("eaU")
-        ebU = ixp.declarerank1("ebU")
-        ecU = ixp.declarerank1("ecU")
+
+        # Eqs 5.7
+        vbU = ixp.zerorank1("vbU")
+        vaU = ixp.zerorank1("vaU")
+        vcU = ixp.zerorank1("vcU")
         vaU[0] = -ymoved
         vaU[1] = xmoved + offset
         vaU[2] = 0
@@ -74,6 +75,13 @@ def WeylScalars():
 
         # TO DO: code up the Levi-Civita tensor
         # Graham-Schmidt orthonormalization of the tetrad
+        waU = ixp.zerorank1("waU")
+        wbU = ixp.zerorank1("wbU")
+        wcU = ixp.zerorank1("wcU")
+        eaU = ixp.zerorank1("eaU")
+        ebU = ixp.zerorank1("ebU")
+        ecU = ixp.zerorank1("ecU")
+
         waU[a] = vaU[a]
         omega11 = 0
         for a in range(DIM):
@@ -103,8 +111,8 @@ def WeylScalars():
         wcU = vcU - omega13*eaU - omega23*ebU
 
         # Construct the tetrad
-        isqrt2 = 1/sqrt(2)
+        isqrt2 = 1/sp.sqrt(2)
         ltetU = isqrt2 * ebU
         ntetU = -isqrt2 * ebU
         mtetU = isqrt2 * (ecU + I*eaU)
-        mtetbarU = conjugate(mtetU)
+        mtetbarU = sp.conjugate(mtetU)
