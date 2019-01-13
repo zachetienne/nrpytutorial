0bf1d440c7af8f824e7f960bc95d759e9ebf1692
Zachariah Etienne <zachetie@gmail.com>
2019-01-13 14:15:54 -0500

    Tutorial-ADM_Initial_Data-Converting_ADMSpherical_or_Cartesian_to_BSSNCurvilinear: New combined module for reading ADM initial data in EITHER Spherical or Cartesian coords and converting them to BSSN Curvilinear initial data.

--- BSSN/ADMSpherical_or_Cartesian_to_BSSNCurvilinear.py
+++ BSSN/ADMSpherical_or_Cartesian_to_BSSNCurvilinear.py
@@ -13,14 +13,15 @@ import indexedexp as ixp
 import reference_metric as rfm
 import BSSN.BSSN_RHSs as bssnrhs # The ConformalFactor parameter is used below
 
-def Convert_Spherical_ADM_to_BSSN_curvilinear(Sph_r_th_ph, gammaSphDD_in, KSphDD_in, alphaSph_in, betaSphU_in, BSphU_in):
-    # This routine convert the ADM variables
-    # $$\left\{\gamma_{ij}, K_{ij}, \alpha, \beta^i\right\}$$
-    # in Spherical coordinates to the BSSN variables
+def Convert_Spherical_or_Cartesian_ADM_to_BSSN_curvilinear(CoordType_in, Sph_r_th_ph_or_Cart_xyz,
+    gammaDD_inSphorCart, KDD_inSphorCart, alpha_inSphorCart, betaU_inSphorCart, BU_inSphorCart):
+    # This routine converts the ADM variables
+    #    $$\left\{\gamma_{ij}, K_{ij}, \alpha, \beta^i\right\}$$
+    #    in Spherical or Cartesian basis+coordinates, first to the BSSN variables
+    #    in the chosen reference_metric::CoordSystem coordinate system+basis:
     # $$\left\{\bar{\gamma}_{i j},\bar{A}_{i j},\phi, K, \bar{\Lambda}^{i}, \alpha, \beta^i, B^i\right\},$$
-    # to the rescaled variables
+    #    and then to the rescaled variables:
     # $$\left\{h_{i j},a_{i j},\phi, K, \lambda^{i}, \alpha, \mathcal{V}^i, \mathcal{B}^i\right\}.$$
-
 
     # The ADM & BSSN formalisms only work in 3D; they are 3+1 decompositions of Einstein's equations.
     #    To implement axisymmetry or spherical symmetry, simply set all spatial derivatives in
@@ -32,17 +33,17 @@ def Convert_Spherical_ADM_to_BSSN_curvilinear(Sph_r_th_ph, gammaSphDD_in, KSphDD
     #    This ensures that the input arrays are not modified below;
     #    modifying them would result in unexpected effects outside
     #    this function.
-    alphaSph = alphaSph_in
-    betaSphU   = ixp.zerorank1()
-    BSphU      = ixp.zerorank1()
-    gammaSphDD = ixp.zerorank2()
-    KSphDD     = ixp.zerorank2()
+    alphaSphorCart = alpha_inSphorCart
+    betaSphorCartU   = ixp.zerorank1()
+    BSphorCartU      = ixp.zerorank1()
+    gammaSphorCartDD = ixp.zerorank2()
+    KSphorCartDD     = ixp.zerorank2()
     for i in range(DIM):
-        betaSphU[i] = betaSphU_in[i]
-        BSphU[i]    = BSphU_in[i]
+        betaSphorCartU[i] = betaU_inSphorCart[i]
+        BSphorCartU[i]    = BU_inSphorCart[i]
         for j in range(DIM):
-            gammaSphDD[i][j] = gammaSphDD_in[i][j]
-            KSphDD[i][j]     = KSphDD_in[i][j]
+            gammaSphorCartDD[i][j] = gammaDD_inSphorCart[i][j]
+            KSphorCartDD[i][j]     = KDD_inSphorCart[i][j]
 
     # Step 1: All input quantities are in terms of r,th,ph. We want them in terms of xx0,xx1,xx2,
     #         so here we call sympify_integers__replace_rthph() to replace r,th,ph with the 
@@ -50,33 +51,44 @@ def Convert_Spherical_ADM_to_BSSN_curvilinear(Sph_r_th_ph, gammaSphDD_in, KSphDD
     #         metric in reference_metric.py's xxSph[] list:
     # Note that substitution only works when the variable is not an integer. Hence the 
     #         if isinstance(...,...) stuff:
-    def sympify_integers__replace_rthph(obj, Sph_r_th_ph, r_th_ph_of_xx):
+    def sympify_integers__replace_rthph_or_Cartxyz(obj, rthph_or_xyz, rthph_or_xyz_of_xx):
         if isinstance(obj, int):
             return sp.sympify(obj)
         else:
-            return obj.subs(Sph_r_th_ph[0], r_th_ph_of_xx[0]).subs(Sph_r_th_ph[1], r_th_ph_of_xx[1]).subs(Sph_r_th_ph[2], r_th_ph_of_xx[2])
+            return obj.subs(rthph_or_xyz[0], rthph_or_xyz_of_xx[0]).\
+                subs(rthph_or_xyz[1], rthph_or_xyz_of_xx[1]).\
+                subs(rthph_or_xyz[2], rthph_or_xyz_of_xx[2])
 
-    alphaSph = sympify_integers__replace_rthph(alphaSph, Sph_r_th_ph, rfm.xxSph)
+    r_th_ph_or_Cart_xyz_of_xx = []
+    if CoordType_in == "Spherical":
+        r_th_ph_or_Cart_xyz_of_xx = rfm.xxSph
+    elif CoordType_in == "Cartesian":
+        r_th_ph_or_Cart_xyz_of_xx = rfm.xxCart
+    else:
+        print("Error: Can only convert ADM Cartesian or Spherical initial data to BSSN Curvilinear coords.")
+        exit(1)
+
+    alphaSphorCart = sympify_integers__replace_rthph_or_Cartxyz(alphaSphorCart, Sph_r_th_ph_or_Cart_xyz, r_th_ph_or_Cart_xyz_of_xx)
     for i in range(DIM):
-        betaSphU[i] = sympify_integers__replace_rthph(betaSphU[i], Sph_r_th_ph, rfm.xxSph)
-        BSphU[i]    = sympify_integers__replace_rthph(BSphU[i],    Sph_r_th_ph, rfm.xxSph)
+        betaSphorCartU[i] = sympify_integers__replace_rthph_or_Cartxyz(betaSphorCartU[i], Sph_r_th_ph_or_Cart_xyz, r_th_ph_or_Cart_xyz_of_xx)
+        BSphorCartU[i]    = sympify_integers__replace_rthph_or_Cartxyz(BSphorCartU[i],    Sph_r_th_ph_or_Cart_xyz, r_th_ph_or_Cart_xyz_of_xx)
         for j in range(DIM):
-            gammaSphDD[i][j] = sympify_integers__replace_rthph(gammaSphDD[i][j], Sph_r_th_ph, rfm.xxSph)
-            KSphDD[i][j]     = sympify_integers__replace_rthph(KSphDD[i][j],     Sph_r_th_ph, rfm.xxSph)
+            gammaSphorCartDD[i][j] = sympify_integers__replace_rthph_or_Cartxyz(gammaSphorCartDD[i][j], Sph_r_th_ph_or_Cart_xyz, r_th_ph_or_Cart_xyz_of_xx)
+            KSphorCartDD[i][j]     = sympify_integers__replace_rthph_or_Cartxyz(KSphorCartDD[i][j],     Sph_r_th_ph_or_Cart_xyz, r_th_ph_or_Cart_xyz_of_xx)
 
     # Step 2: All ADM initial data quantities are now functions of xx0,xx1,xx2, but
-    #         we they are still in the Spherical basis. We can now directly apply 
+    #         we they are still in the Spherical or Cartesian basis. We can now directly apply
     #         Jacobian transformations to get them in the correct xx0,xx1,xx2 basis:
 
     # alpha is a scalar, so no Jacobian transformation is necessary.
-    alpha = alphaSph
+    alpha = alphaSphorCart
 
-    Jac_dUSph_dDrfmUD = ixp.zerorank2()
+    Jac_dUSphorCart_dDrfmUD = ixp.zerorank2()
     for i in range(DIM):
         for j in range(DIM):
-            Jac_dUSph_dDrfmUD[i][j] = sp.diff(rfm.xxSph[i],rfm.xx[j])
+            Jac_dUSphorCart_dDrfmUD[i][j] = sp.diff(r_th_ph_or_Cart_xyz_of_xx[i],rfm.xx[j])
 
-    Jac_dUrfm_dDSphUD, dummyDET = ixp.generic_matrix_inverter3x3(Jac_dUSph_dDrfmUD)
+    Jac_dUrfm_dDSphorCartUD, dummyDET = ixp.generic_matrix_inverter3x3(Jac_dUSphorCart_dDrfmUD)
 
     betaU   = ixp.zerorank1()
     BU      = ixp.zerorank1()
@@ -84,16 +96,17 @@ def Convert_Spherical_ADM_to_BSSN_curvilinear(Sph_r_th_ph, gammaSphDD_in, KSphDD
     KDD     = ixp.zerorank2()
     for i in range(DIM):
         for j in range(DIM):
-            betaU[i] += Jac_dUrfm_dDSphUD[i][j] * betaSphU[j]
-            BU[i]    += Jac_dUrfm_dDSphUD[i][j] * BSphU[j]
+            betaU[i] += Jac_dUrfm_dDSphorCartUD[i][j] * betaSphorCartU[j]
+            BU[i]    += Jac_dUrfm_dDSphorCartUD[i][j] * BSphorCartU[j]
             for k in range(DIM):
                 for l in range(DIM):
-                    gammaDD[i][j] += Jac_dUSph_dDrfmUD[k][i]*Jac_dUSph_dDrfmUD[l][j] * gammaSphDD[k][l]
-                    KDD[i][j]     += Jac_dUSph_dDrfmUD[k][i]*Jac_dUSph_dDrfmUD[l][j] *     KSphDD[k][l]
+                    gammaDD[i][j] += Jac_dUSphorCart_dDrfmUD[k][i]*Jac_dUSphorCart_dDrfmUD[l][j] * gammaSphorCartDD[k][l]
+                    KDD[i][j]     += Jac_dUSphorCart_dDrfmUD[k][i]*Jac_dUSphorCart_dDrfmUD[l][j] *     KSphorCartDD[k][l]
 
-    # Step 3: All ADM quantities were input into this function in the Spherical basis, 
-    #         as functions of r,th,ph. In Steps 1 and 2 above, we have converted them
-    #         to the xx0,xx1,xx2 basis, and as functions of xx0,xx1,xx2.
+
+    # Step 3: All ADM quantities were input into this function in the Spherical or Cartesian
+    #         basis, as functions of r,th,ph or x,y,z, respectively. In Steps 1 and 2 above,
+    #         we converted them to the xx0,xx1,xx2 basis, and as functions of xx0,xx1,xx2.
     #         Here we convert ADM quantities to their BSSN Curvilinear counterparts:
     
     # Step 3.1: Convert ADM $\gamma_{ij}$ to BSSN $\bar{\gamma}_{ij}$:
