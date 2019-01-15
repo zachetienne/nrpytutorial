29ea1450838d39c04b9528357fc5c6bf54e8ea9d
Zachariah Etienne <zachetie@gmail.com>
2019-01-14 23:05:15 -0500

    Start work on TOV interpolator

---
+++ TOV/tov_interp.c
@@ -0,0 +1,139 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "string.h"
+
+#define REAL double
+
+int count_num_lines_in_file(FILE *in1Dpolytrope) {
+  int numlines_in_file = 0;
+  char * line = NULL;
+
+  size_t len = 0;
+  ssize_t read;
+  while ((read = getline(&line, &len, in1Dpolytrope)) != -1) {
+    numlines_in_file++;
+  }
+  rewind(in1Dpolytrope);
+
+  return numlines_in_file;
+}
+
+int read_datafile__set_arrays(FILE *in1Dpolytrope, REAL *r_arr,REAL *rho_arr,REAL *P_arr,REAL *M_arr,REAL *expnu_arr) {
+  char * line = NULL;
+
+  size_t len = 0;
+  ssize_t read;
+
+  int which_line = 0;
+  while ((read = getline(&line, &len, in1Dpolytrope)) != -1) {
+    // Define the line delimiters (i.e., the stuff that goes between the data on a given
+    //     line of data.  Here, we define both spaces " " and tabs "\t" as data delimiters.
+    const char delimiters[] = " \t";
+
+    //Now we define "token", a pointer to the first column of data
+    char *token;
+
+    //Each successive time we call strtok(NULL,blah), we read in a new column of data from
+    //     the originally defined character array, as pointed to by token.
+
+    token=strtok(line, delimiters); if(token==NULL) { printf("BADDDD\n"); return 1; }
+    r_arr[which_line]     = strtod(token, NULL); token = strtok( NULL, delimiters );
+    rho_arr[which_line]   = strtod(token, NULL); token = strtok( NULL, delimiters );
+    P_arr[which_line]     = strtod(token, NULL); token = strtok( NULL, delimiters );
+    M_arr[which_line]     = strtod(token, NULL); token = strtok( NULL, delimiters );
+    expnu_arr[which_line] = strtod(token, NULL);
+
+    which_line++;
+  }
+  return 0;
+}
+
+
+void interpolate_1D(const REAL rr,  const int numlines_in_file,const REAL *r_arr,const REAL *rho_arr,const REAL *P_arr,const REAL *M_arr,const REAL *expnu_arr,
+                    REAL *rho,REAL *P,REAL *M,REAL *expnu) {
+  int bisection_idx_finder(const REAL rr, const int numlines_in_file, const REAL *r_arr) {
+    int x1 = 0;
+    int x2 = numlines_in_file-1;
+    REAL y1 = rr-r_arr[x1];
+    REAL y2 = rr-r_arr[x2];
+    if(y1*y2 >= 0) {
+      fprintf(stderr,"INTERPOLATION BRACKETING ERROR %e | %e %e\n",rr,y1,y2);
+      exit(1);
+    }
+    for(int i=0;i<numlines_in_file;i++) {
+      int x_midpoint = (x1+x2)/2;
+      REAL y_midpoint = rr-r_arr[x_midpoint];
+      if(y_midpoint*y1 < 0) {
+        x2 = x_midpoint;
+        y2 = y_midpoint;
+      } else {
+        x1 = x_midpoint;
+        y1 = y_midpoint;
+      }
+      if( abs(x2-x1) == 1 ) {
+        // If r_arr[x1] is closer to rr than r_arr[x2] then return x1:
+        if(fabs(rr-r_arr[x1]) < fabs(rr-r_arr[x2])) return x1;
+        // Otherwiser return x2:
+        return x2;
+      }
+    }
+    fprintf(stderr,"INTERPOLATION BRACKETING ERROR: DID NOT CONVERGE.\n");
+    exit(1);
+  }
+
+  int idx = bisection_idx_finder(rr,numlines_in_file,r_arr);
+  //printf("%d %e %e %e\n", idx, r_arr[idx-1],rr,r_arr[idx]);
+}
+
+int main() {
+
+  // Open the data file:
+  char filename[100];
+  sprintf(filename,"../outputTOVpolytrope.txt");
+  FILE *in1Dpolytrope = fopen(filename, "r");
+  if (in1Dpolytrope == NULL) {
+    fprintf(stderr,"ERROR: could not open file %s\n",filename);
+    exit(1);
+  }
+
+  // Count the number of lines in the data file:
+  int numlines_in_file = count_num_lines_in_file(in1Dpolytrope);
+
+  // Allocate space for all data arrays:
+  REAL *r_arr       = (REAL *)malloc(sizeof(REAL)*numlines_in_file);
+  REAL *rho_arr     = (REAL *)malloc(sizeof(REAL)*numlines_in_file);
+  REAL *P_arr       = (REAL *)malloc(sizeof(REAL)*numlines_in_file);
+  REAL *M_arr       = (REAL *)malloc(sizeof(REAL)*numlines_in_file);
+  REAL *expnu_arr   = (REAL *)malloc(sizeof(REAL)*numlines_in_file);
+
+  // Read from the data file, filling in arrays
+  if(read_datafile__set_arrays(in1Dpolytrope, r_arr,rho_arr,P_arr,M_arr,expnu_arr) == 1) {
+    fprintf(stderr,"ERROR WHEN READING FILE %s!\n",filename);
+    exit(1);
+  }
+  fclose(in1Dpolytrope);
+
+  // Next, interpolate!
+  // Create trial radius array:
+  int num_r_pts = 100000000;
+  //REAL *r_out_arr = (REAL *)malloc(sizeof(REAL)*num_r_pts);
+  struct drand48_data randBuffer;
+  srand48_r(1313, &randBuffer);
+#pragma omp parallel for
+  for(int i=0;i<num_r_pts;i++) {
+    REAL rr;
+    drand48_r(&randBuffer,&rr);
+    rr *= r_arr[numlines_in_file-1];
+    REAL rho,P,M,expnu;
+    interpolate_1D(rr,  numlines_in_file,r_arr,rho_arr,P_arr,M_arr,expnu_arr,  &rho,&P,&M,&expnu);
+  }
+  
+  free(r_arr);
+  free(rho_arr);
+  free(P_arr);
+  free(M_arr);
+  free(expnu_arr);
+  
+  return 0;
+}
