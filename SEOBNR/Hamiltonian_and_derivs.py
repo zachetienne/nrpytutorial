00830a0db9ad893fae39fd31035e5adacec155e8
Tyler Knowles <tyler.knowles@ligo.org>
2019-09-06 16:34:33 -0400

    SEOBNR: more coefficient updates; ready to compare derivative values to LALSuite.

--- SEOBNR/Hamiltonian_and_derivs.py
+++ SEOBNR/Hamiltonian_and_derivs.py
@@ -206,7 +206,7 @@ def output_H_and_derivs():
 
     with open("SEOBNR_Playground_Pycodes/numpy_expressions.py", "w") as file:
         file.write("import numpy as np\n")
-        file.write("def compute_dHdq(m1, m2, eta, x, y, z, px, py, pz, s1x, s1y, s1z, s2x, s2y, s2z, KK, k0, k1, k2, k3, k4, k5, k5l, d1, d1v2, dheffSS, dheffSSv2, tortoise):\n")
+        file.write("def compute_dHdq(m1, m2, eta, x, y, z, px, py, pz, s1x, s1y, s1z, s2x, s2y, s2z, KK, k0, k1, k2, k3, k4, k5, k5l, d1v2, dheffSSv2, tortoise):\n")
         for i in range(len(lr)-1):
             file.write("\t" + lr[i].lhs + " = " + str(lr[i].rhs).replace("Rational(",
                     "np.divide(").replace("sqrt(", "np.sqrt(").replace("log(", "np.log(").replace("sign(", "np.sign(").replace("Abs(", "np.abs(") + "\n")
--- SEOBNR/constant_coeffs.py
+++ SEOBNR/constant_coeffs.py
@@ -1,22 +1,34 @@
-#Some of these terms are in the Hamiltonian tutorial.  Move them out so we can use them in the initial conditions
-#and not recompute them!
+# Constants of fit to numerical relativity for the spinning effective one-body formulation
 
+# Import necessary NumPy, SymPy, and SEOBNR modules
 import numpy as np
 
+# Compute fits to numerical relativity
 def compute_const_coeffs(eta, gamma, a):
 
+    # Define frequently-used constants
     asq = a*a
     pisq = np.pi*np.pi
 
-    c20 = 1.712
-    c21 = -1.803949138004582
-    c22 = -39.77229225266885
-    c23 = 103.16588921239249
+    # Define constants that determine the fitting parameter K
+    # See the discussion in https://arxiv.org/pdf/1311.2544.pdf between Equations (3) and (4)
+    K0 = 1.712
+    K1 = -1.803949138004582
+    K2 = -39.77229225266885
+    K3 = 103.16588921239249
 
-    K = c20 + c21*eta + c22*eta*eta + c23*eta*eta*eta
+    # Compute the fitting parameter K
+    # See https://arxiv.org/abs/0912.3517 Equation (5.67) and the discussion following Equation 6.9
+    # as well as https://arxiv.org/pdf/1311.2544.pdf
+    K = K0 + K1*eta + K2*eta*eta + K3*eta*eta*eta
+    
+    # Define more frequently-used constants
     EtaKm1 = eta*K - 1.
     EtaKm1sq = EtaKm1*EtaKm1
 
+    # Compute the Post-Newtonian coefficients
+    # See https://arxiv.org/abs/0912.3517 Equations (5.77) to (5.81) and
+    # https://arxiv.org/pdf/1311.2544.pdf Equation (2)
     Delta0 = K*(EtaKm1 - 1.)
     Delta1 = -2.*(Delta0 + K)*EtaKm1
 
@@ -29,20 +41,17 @@ def compute_const_coeffs(eta, gamma, a):
     Delta2sq = Delta2*Delta2
 
     Delta3 = -Delta1cu/3. + Delta1*Delta2 + Delta1sq*EtaKm1 - 2.*(Delta2 - EtaKm1)*EtaKm1 - asq*Delta1*EtaKm1sq
-
     Delta4 = 1./12.*(6*asq*(Delta1sq - 2*Delta2)*EtaKm1sq + 3*Delta1ft - 8*EtaKm1*Delta1cu - 12*Delta2*Delta1sq
                      + 12*(2*EtaKm1*Delta2 + Delta3)*Delta1 + 12*(94./3. - 41./32.*pisq)*EtaKm1sq
                      + 6*(Delta2*Delta2 - 4*Delta3*EtaKm1))
-
     Delta5 = EtaKm1sq*(-4237./60. + 128./5.*gamma + 2275./512.*pisq - asq*(Delta1cu - 3.*Delta1*Delta2 + 3.*Delta3)/3.
                      - (Delta1ft*Delta1 - 5.*Delta1cu*Delta2 + 5.*Delta1*Delta2sq + 5.*Delta1sq*Delta3
-                        - 5.*Delta2*Delta3 - 5.*Delta1*Delta4)/(5.*EtaKm1sq) + (Delta1ft - 4.*(Delta1sq*Delta2)
+                        - 5.*Delta2*Delta3 - 5.*Delta1*Delta4)/(5.*EtaKm1sq) + (Delta1ft - 4.*Delta1sq*Delta2
                         + 2.*Delta2sq + 4.*Delta1*Delta3 - 4.*Delta4)/(2*EtaKm1) + (256./5.)*np.log(2))
     Delta5l = (64./5.)*EtaKm1sq
 
-    d1 = 0.
-    d1v2 = -74.71 - 156.*eta + 627.5*eta*eta
-    dheffSS = 0.
-    dheffSSv2 = 8.127 - 154.2*eta + 830.8*eta*eta
+    #Add comment here
+    dSO = -74.71 - 156.*eta + 627.5*eta*eta
+    dSS = 8.127 - 154.2*eta + 830.8*eta*eta
 
-    return K, Delta0, Delta1, Delta2, Delta3, Delta4, Delta5, Delta5l, d1, d1v2, dheffSS, dheffSSv2
+    return K, Delta0, Delta1, Delta2, Delta3, Delta4, Delta5, Delta5l, dSO, dSS
