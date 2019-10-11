d6957e7813d27c67e4dffe2f445fc4c5e877ea12
Zachariah Etienne <zachetie@gmail.com>
2019-10-11 09:29:01 -0400

    IllinoisGRMHD/doc/generate_IllinoisGRMHD_from_ipynb_files.sh: Automatically generate the thorn in parallel.

--- IllinoisGRMHD/doc/generate_IllinoisGRMHD_from_ipynb_files.sh
+++ IllinoisGRMHD/doc/generate_IllinoisGRMHD_from_ipynb_files.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+NUM_JOBS=8
+
+count=0
 for i in *.ipynb; do
     echo Executing $i ...
-    jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=-1 $i
+    jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=-1 $i &
+    if ((count==$NUM_JOBS)); then
+        wait
+        count=0
+    else
+        let count+=1
+    fi
 done
 
 echo Finished!
+
+# Alternative approach if gnu parallel is installed:
+# rm -f /tmp/joblist.txt
+# for i in *.ipynb; do
+#     echo jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=-1 $i >> /tmp/joblist.txt
+# done
+# parallel --jobs 8 < /tmp/joblist.txt
