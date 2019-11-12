37151155ec1a421ea6309b6ae015a1b5bf9fa833
Zachariah Etienne <zachetie@gmail.com>
2019-11-11 21:49:41 -0500

    run_Jupyter_notebook.sh: Force Jupyter to run the notebook even if the kernel used to generate it is different than what saved it last

--- run_Jupyter_notebook.sh
+++ run_Jupyter_notebook.sh
@@ -12,4 +12,15 @@ if [ ! -f $1 ]; then
     exit
 fi
 
+# NRPy+ Jupyter notebooks are completely Python 2/3 cross-compatible.
+#   However `jupyter nbconvert` will refuse to run if the notebook
+#   was generated using a different kernel. Here we fool Jupyter
+#   to think the notebook was written using the native python kernel.
+PYTHONMAJORVERSION=`python -c "import sys;print(sys.version_info[0])"`
+if (( $PYTHONMAJORVERSION == 3 )); then
+    cat $1 | sed "s/   \"name\": \"python2\"/   \"name\": \"python3\"/g" > $1-tmp ; mv $1-tmp $1
+else
+    cat $1 | sed "s/   \"name\": \"python3\"/   \"name\": \"python2\"/g" > $1-tmp ; mv $1-tmp $1
+fi
+
 time jupyter nbconvert --to notebook --inplace --execute --ExecutePreprocessor.timeout=-1 $1
