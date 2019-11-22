b77cef0d963e9684e18aba3863f37ec0fcb9d823
Zachariah Etienne <zachetie@gmail.com>
2019-11-21 22:44:31 -0500

    UnitTesting/run_NRPy_UnitTests.sh: uncomment the non-GRHD unit tests :)

--- UnitTesting/run_NRPy_UnitTests.sh
+++ UnitTesting/run_NRPy_UnitTests.sh
@@ -39,19 +39,19 @@ rerun_if_fail=false
 
 $PYTHONEXEC UnitTesting/Test_UnitTesting/test_functions.py
 
-# add_test UnitTesting/Test_UnitTesting/test_module.py
-# add_test BSSN/tests/test_BSSN.py
+add_test UnitTesting/Test_UnitTesting/test_module.py
+add_test BSSN/tests/test_BSSN.py
 add_test GRHD/tests/test_GRHD.py
-# add_test GRFFE/tests/test_GRFFE.py
-# add_test FishboneMoncriefID/tests/test_FishboneMoncriefID.py
-# add_test in_progress/GiRaFFE_HO/tests/test_GiRaFFE_HO.py
-# add_test in_progress/GiRaFFEfood_HO/tests/test_GiRaFFEfood_HO.py
-# add_test Maxwell/tests/test_Maxwell.py
-# add_test ScalarWave/tests/test_ScalarWave.py
-# add_test ScalarWaveCurvilinear/tests/test_ScalarWaveCurvilinear.py
-# add_test tests/test_reference_metric.py
-# add_test u0_smallb_Poynting__Cartesian/tests/test_u0_smallb_Poynting__Cartesian.py
-# add_test WeylScal4NRPy/tests/test_WeylScal4NRPy.py
+add_test GRFFE/tests/test_GRFFE.py
+add_test FishboneMoncriefID/tests/test_FishboneMoncriefID.py
+add_test in_progress/GiRaFFE_HO/tests/test_GiRaFFE_HO.py
+add_test in_progress/GiRaFFEfood_HO/tests/test_GiRaFFEfood_HO.py
+add_test Maxwell/tests/test_Maxwell.py
+add_test ScalarWave/tests/test_ScalarWave.py
+add_test ScalarWaveCurvilinear/tests/test_ScalarWaveCurvilinear.py
+add_test tests/test_reference_metric.py
+add_test u0_smallb_Poynting__Cartesian/tests/test_u0_smallb_Poynting__Cartesian.py
+add_test WeylScal4NRPy/tests/test_WeylScal4NRPy.py
 
 # TODO: add your tests here
 
