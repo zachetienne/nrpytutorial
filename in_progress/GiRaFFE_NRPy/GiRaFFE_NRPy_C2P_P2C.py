9832d4421a5a3a10872f08fc4a36ccb2e9c13096
Patrick Nelson <penelson@mix.wvu.edu>
2019-12-16 12:51:48 -0500

    GiRaFFE_NRPy_C2P_P2C: Updated the module, tutorial, and unit test to use the new GRFFE function.

--- in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_C2P_P2C.py
+++ in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_C2P_P2C.py
@@ -140,15 +140,10 @@ def GiRaFFE_NRPy_P2C(gammadet,gammaDD,betaU,alpha,  ValenciavU,BU, sqrt4pi):
     # First compute stress-energy tensor T4UU and T4UD:
     
     GRHD.u4U_in_terms_of_ValenciavU__rescale_ValenciavU_by_applying_speed_limit(alpha, betaU, gammaDD, ValenciavU)
-    smallb4U = ixp.zerorank1(DIM=4)
-    smallb4U[0] = 0
-    for i in range(3):
-        smallb4U[i+1] = BU[i] / (sqrt4pi*alpha*GRHD.u4U_ito_ValenciavU[0])
-    
-#     GRFFE.compute_smallb4U(gammaDD, betaU, alpha, GRHD.u4U_ito_ValenciavU, BU, sqrt4pi)
-    GRFFE.compute_smallbsquared(gammaDD, betaU, alpha, smallb4U)
+    GRFFE.compute_smallb4U_with_driftvU_for_FFE(gammaDD, betaU, alpha, GRHD.u4U_ito_ValenciavU, BU, sqrt4pi)
+    GRFFE.compute_smallbsquared(gammaDD, betaU, alpha, GRFFE.smallb4_driftU)
 
-    GRFFE.compute_TEM4UU(gammaDD, betaU, alpha, smallb4U, GRFFE.smallbsquared, GRHD.u4U_ito_ValenciavU)
+    GRFFE.compute_TEM4UU(gammaDD, betaU, alpha, GRFFE.smallb4_driftU, GRFFE.smallbsquared, GRHD.u4U_ito_ValenciavU)
     GRFFE.compute_TEM4UD(gammaDD, betaU, alpha, GRFFE.TEM4UU)
 
     # Compute conservative variables in terms of primitive variables
