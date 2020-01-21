6b49f99b786d2a2a970e8e21a149222bfc97bbcc
Patrick Nelson <penelson@mix.wvu.edu>
2020-01-21 15:12:01 -0500

    GiRaFFE_NRPy_FCVAL: Polish unit test and take advantage of outCfunction().

--- in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_FCVAL.py
+++ in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_FCVAL.py
@@ -5,6 +5,7 @@ nrpy_dir_path = os.path.join("..")
 if nrpy_dir_path not in sys.path:
     sys.path.append(nrpy_dir_path)
 
+from outputC import *            # NRPy+: Core C code output module
 import cmdline_helper as cmd     # NRPy+: Multi-platform Python command-line interface
 global GiRaFFE_NRPy_FCVAL
 
@@ -52,25 +53,34 @@ const int num_metric_gfs = 10;
 const int kronecker_delta[3][3] = { { 1,0,0 },
                                     { 0,1,0 },
                                     { 0,0,1 } };
+""")
+        
+    desc = "Interpolate metric gridfunctions to cell faces"
+    name = "interpolate_metric_gfs_to_cell_faces"
+    interp_Cfunc = outCfunction(
+        outfile  = "returnstring", desc=desc, name=name,
+        params   ="const paramstruct *params,REAL *auxevol_gfs,const int flux_dirn",
+        preloop  ="""    int in_gf,out_gf;
+    REAL Qm2,Qm1,Qp0,Qp1;
 
-void interpolate_metric_gfs_to_cell_faces(const paramstruct *params,REAL *auxevol_gfs,const int flux_dirn) {
-#include "set_Cparameters.h"
- 
-#pragma omp parallel for
-    for(int gf = 0;gf < num_metric_gfs;gf++) {
-        int in_gf  = metric_gfs_list[gf];
-        int out_gf = metric_gfs_face_list[gf];
+""" ,
+        body     ="""    for(int gf = 0;gf < num_metric_gfs;gf++) {
+        in_gf  = metric_gfs_list[gf];
+        out_gf = metric_gfs_face_list[gf];
         for (int i2 = NGHOSTS;i2 < Nxx2+NGHOSTS+1;i2++) {
             for (int i1 = NGHOSTS;i1 < Nxx2+NGHOSTS+1;i1++) {
                 for (int i0 = NGHOSTS;i0 < Nxx2+NGHOSTS+1;i0++) {
-                    REAL Qm2 = auxevol_gfs[IDX4S(in_gf,i0-2*kronecker_delta[0][flux_dirn],i1-2*kronecker_delta[1][flux_dirn],i2-2*kronecker_delta[2][flux_dirn])];
-                    REAL Qm1 = auxevol_gfs[IDX4S(in_gf,i0-kronecker_delta[0][flux_dirn],i1-kronecker_delta[1][flux_dirn],i2-kronecker_delta[2][flux_dirn])];
-                    REAL Qp0 = auxevol_gfs[IDX4S(in_gf,i0,i1,i2)];
-                    REAL Qp1 = auxevol_gfs[IDX4S(in_gf,i0+kronecker_delta[0][flux_dirn],i1+kronecker_delta[1][flux_dirn],i2+kronecker_delta[2][flux_dirn])];
+                    Qm2 = auxevol_gfs[IDX4S(in_gf,i0-2*kronecker_delta[0][flux_dirn],i1-2*kronecker_delta[1][flux_dirn],i2-2*kronecker_delta[2][flux_dirn])];
+                    Qm1 = auxevol_gfs[IDX4S(in_gf,i0-kronecker_delta[0][flux_dirn],i1-kronecker_delta[1][flux_dirn],i2-kronecker_delta[2][flux_dirn])];
+                    Qp0 = auxevol_gfs[IDX4S(in_gf,i0,i1,i2)];
+                    Qp1 = auxevol_gfs[IDX4S(in_gf,i0+kronecker_delta[0][flux_dirn],i1+kronecker_delta[1][flux_dirn],i2+kronecker_delta[2][flux_dirn])];
                     auxevol_gfs[IDX4S(out_gf,i0,i1,i2)] = COMPUTE_FCVAL(Qm2,Qm1,Qp0,Qp1);
                 }
             }
         }
     }
-}
+
 """)
+
+    with open(os.path.join(Ccodesdir,"interpolate_metric_gfs_to_cell_faces.h"),"a") as file:
+        file.write(interp_Cfunc)
