b9bd2c955338368aac172f7ed0bb243861c6e840
Zachariah Etienne <zachetie@gmail.com>
2020-04-05 19:59:16 -0400

    .travis.yml ; UnitTesting/core_Jupyter_notebook_testsuite.sh: Run all but start-to-finish tutorial notebooks as validation; include Python 2.7

--- UnitTesting/core_Jupyter_notebook_testsuite.sh
+++ UnitTesting/core_Jupyter_notebook_testsuite.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-./run_Jupyter_notebook.sh Tutorial-Finite_Difference_Derivatives.ipynb && git diff Tutorial-Finite_Difference_Derivatives.ipynb && \
-./run_Jupyter_notebook.sh Tutorial-Numerical_Grids.ipynb && git diff Tutorial-Numerical_Grids.ipynb && \
-./run_Jupyter_notebook.sh Tutorial-Coutput__Parameter_Interface.ipynb && git diff Tutorial-Coutput__Parameter_Interface.ipynb && \
-./run_Jupyter_notebook.sh Tutorial-cmdline_helper.ipynb && git diff Tutorial-cmdline_helper.ipynb && \
-./run_Jupyter_notebook.sh Tutorial-Symbolic_Tensor_Rotation.ipynb && git diff Tutorial-Symbolic_Tensor_Rotation.ipynb
+for i in Tutorial-[A-RT-Z]*.ipynb Tutorial-S[A-SU-Z]*.ipynb; do
+    ./run_Jupyter_notebook.sh $i && git diff $i
+done
+# ./run_Jupyter_notebook.sh Tutorial-Finite_Difference_Derivatives.ipynb && git diff Tutorial-Finite_Difference_Derivatives.ipynb && \
+# ./run_Jupyter_notebook.sh Tutorial-Numerical_Grids.ipynb && git diff Tutorial-Numerical_Grids.ipynb && \
+# ./run_Jupyter_notebook.sh Tutorial-Coutput__Parameter_Interface.ipynb && git diff Tutorial-Coutput__Parameter_Interface.ipynb && \
+# ./run_Jupyter_notebook.sh Tutorial-cmdline_helper.ipynb && git diff Tutorial-cmdline_helper.ipynb && \
+# ./run_Jupyter_notebook.sh Tutorial-Symbolic_Tensor_Rotation.ipynb && git diff Tutorial-Symbolic_Tensor_Rotation.ipynb
