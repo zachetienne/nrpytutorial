b95bfff538dd40bc75ddc05ea2e980748cb901bd
Zachariah Etienne <zachetie@gmail.com>
2020-04-05 21:39:09 -0400

    Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb: Add additional debug info in case validation test fails

--- UnitTesting/core_Jupyter_notebook_testsuite.sh
+++ UnitTesting/core_Jupyter_notebook_testsuite.sh
@@ -5,6 +5,7 @@ set -e # Error out if any commands complete with an error.
 # Skip Baikal and 
 for i in Tutorial-[A]*.ipynb Tutorial-[C-RT-Z]*.ipynb Tutorial-B[B-Z]*.ipynb Tutorial-S[A-SU-Z]*.ipynb; do
     ./run_Jupyter_notebook.sh $i notimer
+    cat $i | sed "s/\\\r\\\n/\\\n/g" > $i-new && mv $i-new $i
     git diff $i
 done
 # ./run_Jupyter_notebook.sh Tutorial-Finite_Difference_Derivatives.ipynb && git diff Tutorial-Finite_Difference_Derivatives.ipynb && \
