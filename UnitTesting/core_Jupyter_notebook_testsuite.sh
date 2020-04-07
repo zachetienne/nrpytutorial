93dd65b3a2706155544410ca4df87b4819cbfa08
Zachariah Etienne <zachetie@gmail.com>
2020-04-07 12:54:39 -0400

    UnitTesting/core_Jupyter_notebook_testsuite.sh: Clean up git diff a bit.

--- UnitTesting/core_Jupyter_notebook_testsuite.sh
+++ UnitTesting/core_Jupyter_notebook_testsuite.sh
@@ -8,7 +8,9 @@ set -e # Error out if any commands complete with an error.
 for i in Tutorial-[A]*.ipynb Tutorial-[C-RT-Z]*.ipynb Tutorial-B[B-Z]*.ipynb Tutorial-S[A-SU-Z]*.ipynb Tutorial-Start_to_Finish-*[^4].ipynb; do
     ./run_Jupyter_notebook.sh $i notimer
     cat $i | sed "s/\\\r\\\n/\\\n/g" > $i-new && mv $i-new $i
-    git diff $i | grep -v "image/png" | cdiff | cat
+    git diff $i |grep -v "image/png"|grep -E "^\-|^\+"|grep -v  '^\-\-\-'|cdiff |cat
+#    git diff $i | grep -v "image/png" | cdiff | cat
+#    echo Number of lines different in the git diff: `git diff|grep -v image/png|wc -l`
 done
 # ./run_Jupyter_notebook.sh Tutorial-Finite_Difference_Derivatives.ipynb && git diff Tutorial-Finite_Difference_Derivatives.ipynb && \
 # ./run_Jupyter_notebook.sh Tutorial-Numerical_Grids.ipynb && git diff Tutorial-Numerical_Grids.ipynb && \
