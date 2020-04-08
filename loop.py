cb3c19f28ed3b2b9396c5f1908b3458969853f73
Ken Sible <kjs0001@mix.wvu.edu>
2020-04-08 17:48:04 -0400

    loop.py: added loop tiling and rewritten code

--- loop.py
+++ loop.py
@@ -1,26 +1,22 @@
 # This module contains infrastructure for generating
 #   C-code loops of arbitrary dimension
 
-# Author: Zachariah B. Etienne
+# Primary Author: Zachariah B. Etienne
 #         zachetie **at** gmail **dot* com
+# Secondary Author: Ken Sible
+#         ksible **at** outlook **dot* com
 
-import sys                         # Standard Python modules for multiplatform OS-level functions
+import sys  # Standard Python modules for multiplatform OS-level functions
 
 # loop1D() is just a special case of loop(), and in fact is called by loop().
 #   For documentation on the inputs, see loop()'s documentation below.
-def loop1D(idxvar="i0",lower="0",upper="Nx0",incr="1",OpenMPpragma="#pragma omp parallel for",tabprefix=""):
-    if not (isinstance(idxvar, str) and isinstance(lower, str) and
-            isinstance(upper, str) and isinstance(incr, str) and isinstance(OpenMPpragma, str)):
-        print("Error: all inputs to loop1D() must be STRINGS, and to loop() must be LISTS OF STRINGS")
-        sys.exit(1)
-    OMPheader = ""
-    if OpenMPpragma != "":
-        OMPheader = OpenMPpragma + "\n"
-    incrstr = "++"
-    if incr != "1":
-        incrstr = "+="+incr
-    loopbody = tabprefix+"for(int "+idxvar+"="+lower+"; "+idxvar+"<"+upper+"; "+idxvar+incrstr+")"
-    return OMPheader+loopbody+" {\n", tabprefix+"} // END LOOP: "+loopbody.replace(tabprefix,"")+"\n"
+def loop1D(idxvar="i0", lower="0", upper="Nx0", incr="1", OpenMPpragma="#pragma omp parallel for", tabprefix=""):
+    if not (all(isinstance(i, str) for i in (idxvar, lower, upper, incr, OpenMPpragma))):
+        raise Exception('all list parameters must be strings.')
+    OMPheader = OpenMPpragma + '\n' if OpenMPpragma else ''
+    incrstr = '+=' + incr if incr != '1' else '++'
+    loopbody = tabprefix + 'for(int ' + idxvar + '=' + lower + '; ' + idxvar + '<' + upper + '; ' + idxvar + incrstr + ')'
+    return OMPheader + loopbody + ' {\n', tabprefix + '} // END LOOP: ' + loopbody.replace(tabprefix, '') + '\n'
 
 # loop() creates a C code loop, taking as input:
 #    idxvar (string or list of strings): the index variable name or list of names.
@@ -38,7 +34,8 @@ def loop1D(idxvar="i0",lower="0",upper="Nx0",incr="1",OpenMPpragma="#pragma omp 
 #    OpenMPpragma (string or list of strings): Defined similarly to "lower", except
 #         this refers to the OpenMP pragma corresponding to idxvar or idxvar[i].
 #    tabprefix (optional; string): Sets the tab stop for the C code.
-#    loopguts (optional; string): The loop interior.
+#    loopbody (optional; string): The loop interior.
+#    tilesize (optional; string): The cache block size for loop tiling.
 #
 # Example: loop(["i0","i1"],["0","5"],["N","N-5"],["1","5"],["#pragma omp parallel for",""],
 #               "double a=-2;\n gf[IDX3(GF,i0,i1)]=-2*a;\n"])
@@ -50,54 +47,39 @@ def loop1D(idxvar="i0",lower="0",upper="Nx0",incr="1",OpenMPpragma="#pragma omp 
 #         gf[IDX3(GF,i0,i1)]=-2*a;
 #     } // END LOOP: for(int i1=5;i1<N-5;i1+=5)
 # } // END LOOP: for(int i0=0;i0<N;i0++)
-def loop(idxvar,lower,upper,incr,OpenMPpragma,tabprefix="",loopguts=""):
-    # Step 1: Check and/or clean input. 
-    # Step 1a: If only strings are passed, then create lists out of them:
-    if (isinstance(idxvar,str) and isinstance(lower,str) and
-        isinstance(upper, str) and isinstance(incr, str) and isinstance(OpenMPpragma, str)):
-        idxvar = [idxvar]
-        lower = [lower]
-        upper = [upper]
-        incr = [incr]
-        OpenMPpragma = [OpenMPpragma]
-    # Step 1b: At this point all inputs should be lists. If they are not, then exit.
-    if not (isinstance(idxvar,list) and isinstance(lower,list) and
-            isinstance(upper, list) and isinstance(incr, list) and isinstance(OpenMPpragma, list)):
-        print("Error: loop(idxvar,lower,upper,incr,OpenMPpragma) requires all inputs be lists")
-        sys.exit(1)
-    # Step 1c: At this point all inputs should be lists. If they are not the same length, then exit.
-    if len(idxvar) != len(lower) or len(lower) != len(upper) or len(upper) != len(incr) or len(incr) != len(
-            OpenMPpragma):
-        print("Error: loop(idxvar,lower,upper,incr,OpenMPpragma) requires all inputs be lists OF THE SAME LENGTH")
-        sys.exit(1)
-    # Step 2: tabprefix will be set according to the loop nesting, so the loop has proper tabination;
-    #         one tab for each nesting of the loop.
-    # Step 3: header will be the top of the loop
-    header = ""
-    # Step 4: footerarray 
-    footerarray = []
-    for i in range(len(idxvar)):
-        headerstr,footerstr = loop1D(idxvar[i],lower[i],upper[i],incr[i],OpenMPpragma[i],tabprefix)
-        header += headerstr
-        footerarray.append(footerstr)
-        tabprefix += "    "
-    loopgutsout = ""
-    if loopguts != "":
-        loopgutsarray = loopguts.split("\n")
-        for line in loopgutsarray:
-            loopgutsout += tabprefix+line+"\n"
-    footer = ""
-    for i in range(len(idxvar)-1,-1,-1):
-        footer += footerarray[i]
-    if loopguts == "":
-        return header,footer
-    return header+loopgutsout+footer
+def loop(idxvar, lower, upper, incr, OpenMPpragma, tabprefix="", loopbody="", tilesize=""):
+    if (all(isinstance(i, str) for i in (idxvar, lower, upper, incr, OpenMPpragma))):
+        idxvar, lower, upper, incr, OpenMPpragma = [idxvar], [lower], [upper], [incr], [OpenMPpragma]
+    list_len = len(idxvar)
+    if any(len(i) != list_len for i in (lower, upper, incr, OpenMPpragma)):
+        raise Exception('all list parameters must be the same length.')
+    if tilesize:
+        if isinstance(tilesize, str): tilesize = [tilesize]
+        if len(tilesize) != list_len:
+            raise Exception('tile size must be the same length as all other list parameters.')
+    header_list, footer_list = [], []
+    for i in range(list_len):
+        if len(tilesize) > 0:
+            ext_header, ext_footer = loop1D(idxvar[i] + 'B', lower[i], upper[i], tilesize[i], '', tabprefix + i*'    ')
+            header, footer = loop1D(idxvar[i], idxvar[i] + 'B', 'MIN(%s,%s+%s)' % (upper[i], idxvar[i] + 'B', \
+                tilesize[i]), incr[i], OpenMPpragma[i], tabprefix + (list_len + i)*'    ')
+            header_list.insert(i, ext_header)
+            footer_list.insert(i, ext_footer)
+        else:
+            header, footer = loop1D(idxvar[i], lower[i], upper[i], incr[i], OpenMPpragma[i], tabprefix + i*'    ')
+        header_list.append(header)
+        footer_list.append(footer)
+    if loopbody:
+        loopbody = [tabprefix + (list_len + len(tilesize))*'    ' + line + '\n' for line in loopbody.split('\n')]
+    header = ''.join(header_list)
+    footer = ''.join(footer_list[::-1])
+    if not loopbody: return header, footer
+    return header + ''.join(loopbody) + footer
 
 # Automatic generation of C-code loops around an arbitrarily
 #     defined loop body.
-def simple_loop(loopopts, body):
-    if loopopts == "":
-        return body
+def simple_loop(loopopts, loopbody):
+    if not loopopts: return loopbody
 
     if "AllPoints" in loopopts:
         i2i1i0_mins = ["0", "0", "0"]
@@ -109,9 +91,7 @@ def simple_loop(loopopts, body):
         i2i1i0_maxs = ["NGHOSTS+Nxx2","NGHOSTS+Nxx1","NGHOSTS+Nxx0"]
         if "oldloops" in loopopts:
             i2i1i0_maxs = ["NGHOSTS+Nxx[2]", "NGHOSTS+Nxx[1]", "NGHOSTS+Nxx[0]"]
-    else:
-        print("Error: loopopts given, but no points over which to loop were specified.")
-        sys.exit(1)
+    else: raise Exception('no points over which to loop were specified.')
 
     Read_1Darrays = ["", "", ""]
     if "Read_xxs" in loopopts:
@@ -119,14 +99,11 @@ def simple_loop(loopopts, body):
             Read_1Darrays = ["const REAL xx0 = xx[0][i0];",
                              "            const REAL xx1 = xx[1][i1];",
                              "        const REAL xx2 = xx[2][i2];", ]
-        else:
-            print("Error: No SIMD support on Read_xxs yet.")
-            sys.exit(1)
+        else: raise Exception('no SIMD support on Read_xxs (currently).')
 
     if "Enable_rfm_precompute" in loopopts:
         if "Read_xxs" in loopopts:
-            print("Error: Enable_rfm_precompute and Read_xxs cannot both be enabled.")
-            sys.exit(1)
+            raise Exception('Enable_rfm_precompute and Read_xxs cannot both be enabled.')
         if "EnableSIMD" in loopopts:
             Read_1Darrays = ["#include \"rfm_files/rfm_struct__SIMD_inner_read0.h\"",
                              "#include \"rfm_files/rfm_struct__SIMD_outer_read1.h\"",
@@ -136,20 +113,9 @@ def simple_loop(loopopts, body):
                              "#include \"rfm_files/rfm_struct__read1.h\"",
                              "#include \"rfm_files/rfm_struct__read2.h\""]
 
-    OpenMPpragma = "#pragma omp parallel for"
-    if "DisableOpenMP" in loopopts:
-        OpenMPpragma = ""
+    OpenMPpragma   = "" if "DisableOpenMP" in loopopts else "#pragma omp parallel for"
+    loopincrements = ["1", "1", "SIMD_width"] if "EnableSIMD" in loopopts else ["1","1","1"]
+    tilesize       = ["16", "16", "16"] if "EnableLoopTiling" in loopopts else ""
 
-    loopincrements = ["1","1","1"]
-    if "EnableSIMD" in loopopts:
-        loopincrements = ["1", "1", "SIMD_width"]
-
-    return loop(["i2","i1","i0"],i2i1i0_mins,i2i1i0_maxs,loopincrements,
-                [OpenMPpragma,Read_1Darrays[2],Read_1Darrays[1]],tabprefix="    ",loopguts = Read_1Darrays[0]+"\n"+body)
-
-
-#loopheader,loopfooter = loop(idxvar=["i0","i1"],lower=["0","0"],upper=["Nx0","Nx1"],incr=["1","1"],
-#                             OpenMPpragma=["", "#pragma omp parallel for"])
-#loopheader,loopfooter = loop(idxvar=["i0","i1"],lower=["0","0"],upper=["Nx0","Nx1"],incr=["1","1"],
-#                             OpenMPpragma=["", "#pragma omp parallel for"])
-#print(loopheader+loopfooter)
+    return loop(["i2","i1","i0"], i2i1i0_mins, i2i1i0_maxs, loopincrements, [OpenMPpragma, Read_1Darrays[2], Read_1Darrays[1]], \
+                tabprefix="    ", loopbody=Read_1Darrays[0] + "\n" + loopbody, tilesize=tilesize)
