a05d34c5e413ca3ff1122bead212880a772494dc
Zachariah Etienne <zachetie@gmail.com>
2020-05-08 21:19:34 -0400

    NRPyPN: Validated Post-Newtonian expressions for output to Mathematica, SymPy, or optimized C code. Start work on this.

---
+++ NRPyPN/NRPyPN_shortcuts.py
@@ -0,0 +1,98 @@
+# As documented in the NRPyPN notebook
+# NRPyPN_shortcuts.ipynb, this Python script 
+# provides useful shortcuts for inputting
+# post-Newtonian expressions into SymPy/NRPy+
+
+# Basic functions:
+# dot(a,b): 3-vector dot product
+# cross(a,b): 3-vector cross product
+# define_LeviCivitaSymbol_rank3_dim3(): Outputs Levi-Civita symbol in 3 dimensions
+# div(a,b): a shortcut for SymPy's sp.Rational(a,b), to declare rational numbers
+# num_eval(expr): Numerically evaluates NRPyPN expressions
+
+# Author:  Zach Etienne
+#          zachetie **at** gmail **dot* com
+
+# Step 0: Add NRPy's directory to the path
+# https://stackoverflow.com/questions/16780014/import-file-from-parent-directory
+import os,sys                    # Standard Python modules for multiplatform OS-level functions
+nrpy_dir_path = os.path.join("..")
+if nrpy_dir_path not in sys.path:
+    sys.path.append(nrpy_dir_path)
+import sympy as sp               # SymPy: The Python computer algebra package upon which NRPy+ depends
+import indexedexp as ixp         # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
+
+# Step 1: Define the rank-3 version of the Levi-Civita symbol. Amongst
+#         other uses, this is needed for the construction of the approximate 
+#         quasi-Kinnersley tetrad.
+def define_LeviCivitaSymbol_rank3_dim3():
+    LeviCivitaSymbol = ixp.zerorank3()
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                # From https://codegolf.stackexchange.com/questions/160359/levi-civita-symbol :
+                LeviCivitaSymbol[i][j][k] = (i - j) * (j - k) * (k - i) * sp.Rational(1,2)
+    return LeviCivitaSymbol
+
+# Step 2: Define dot and cross product of vectors
+def dot(vec1,vec2):
+    vec1_dot_vec2 = sp.sympify(0)
+    for i in range(3):
+        vec1_dot_vec2 += vec1[i]*vec2[i]
+    return vec1_dot_vec2
+
+def cross(vec1,vec2):
+    vec1_cross_vec2 = ixp.zerorank1()
+    LeviCivitaSymbol = define_LeviCivitaSymbol_rank3_dim3()
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                vec1_cross_vec2[i] += LeviCivitaSymbol[i][j][k]*vec1[j]*vec2[k]
+    return vec1_cross_vec2
+
+# Step 3: Construct rational numbers a/b via div(a,b)
+def div(a,b):
+    return sp.Rational(a,b)
+
+# Step 4: num_eval(expr), a means to numerically evaluate SymPy/NRPyPN
+#         expressions
+def num_eval(expr, 
+             qmassratio =  1.0,  # must be >= 1
+             nr         = 12.0,  # Orbital separation
+             nchi1x     = +0.,
+             nchi1y     = +0.,
+             nchi1z     = +0.,
+             nchi2x     = +0.,
+             nchi2y     = +0.,
+             nchi2z     = +0.,
+             nPt=None, ndrdt=None):
+
+    # DERIVED QUANTITIES BELOW
+    # We want m1+m2 = 1, so that
+    #         m2/m1 = qmassratio
+    # We find below:
+    nm1   =          1/(1+qmassratio)
+    nm2   = qmassratio/(1+qmassratio)
+    # This way nm1+nm2 = (qmassratio+1)/(1+qmassratio) = 1 CHECK
+    #      and nm2/nm1 = qmassratio                        CHECK
+    
+    nS1U0 = nchi1x*nm1**2
+    nS1U1 = nchi1y*nm1**2
+    nS1U2 = nchi1z*nm1**2
+    nS2U0 = nchi2x*nm2**2
+    nS2U1 = nchi2y*nm2**2
+    nS2U2 = nchi2z*nm2**2
+
+    if nPt != None:
+        expr2 = expr.subs(Pt,nPt)
+        expr  = expr2
+    if ndrdt != None:
+        expr2 = expr.subs(drdt,ndrdt)
+        expr  = expr2
+    return expr\
+.subs(m1,nm1).subs(m2,nm2)\
+.subs(S1U[0],nS1U0).subs(S1U[1],nS1U1).subs(S1U[2],nS1U2)\
+.subs(S2U[0],nS2U0).subs(S2U[1],nS2U1).subs(S2U[2],nS2U2)\
+.subs(chi1U[0],nchi1x).subs(chi1U[1],nchi1y).subs(chi1U[2],nchi1z)\
+.subs(chi2U[0],nchi2x).subs(chi2U[1],nchi2y).subs(chi2U[2],nchi2z)\
+.subs(r,nr).subs(q,nr).subs(sp.pi,sp.N(sp.pi))
