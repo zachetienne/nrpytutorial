61cb9fc8d4dc77b0aa7bea5253d9a50e1b090668
Zachariah Etienne <zachetie@gmail.com>
2020-05-10 17:44:44 -0400

    NRPyPN: PN-Hamiltonian-SSS now fully validated

--- NRPyPN/PN_Hamiltonian_SSS.py
+++ NRPyPN/PN_Hamiltonian_SSS.py
@@ -28,21 +28,21 @@ from NRPyPN_shortcuts import *   # NRPyPN: shortcuts for e.g., vector operations
 # Step 1: 3PN spin-spin-spin term, from Eq. 3.12 of
 #        Levi and Steinhoff (2015):
 #     https://arxiv.org/abs/1410.2601
-def f_H_SSS_3PN(m1,m2, n12U,n21U, S1U,S2U, p1U,p2U, q):
-    def SHS2015_HSSS_3PN_pt(m1,m2, nU, S1U, p1U,p2U, q):
-        p2_minus_m2_over_4m1_p1 = ixp.zerorank1()
+def f_H_SSS_3PN(m1,m2, n12U,n21U, S1U,S2U, p1U,p2U, r12):
+    def f_H_SSS_3PN_pt(m1,m2, nU, S1U,S2U, p1U,p2U, r):
+        p2_minus_m2_over_4m1_p1  = ixp.zerorank1()
         for i in range(3):
-            p2_minus_m2_over_4m1_p1[i] = p2U[i] - m2/(4*m1)*p1U[i]
-        H = ( div(3,2)*(+  dot(S1U,S1U)*dot(S2U,cross(nU,p1U))   # line 1
-                        +  dot(S1U,nU)*dot(S2U,cross(S1U,p1U))   # line 1
-                        -5*dot(S1U,nU)**2*dot(S2U,cross(nU,p1U)) # line 1
-                        +dot(nU,cross(S1U,S2U))*(dot(S1U,p1U)-5*dot(S1U,nU)*dot(p1U,nU)) # line 2
-                        -div(3,2)*m1/m2*(+  dot(S1U,S1U)*dot(S2U,cross(nU,p2U))     # line 3
-                                         +2*dot(S1U,nU)*dot(S2U,cross(S1U,p2U))     # line 3
-                                         -5*dot(S1U,nU)**2*dot(S2U,cross(nU,p2U)))) # line 3
-              -dot(cross(S1U,nU), p2_minus_m2_over_4m1_p1)*(dot(S1U,S1U) - 5*dot(S1U,nU)**2) )/q**4
-        return H
-
+            p2_minus_m2_over_4m1_p1[i]  = p2U[i] - m2/(4*m1)*p1U[i]
+        H_SSS_3PN_pt = (+div(3,2)*(+dot(S1U,S1U)*dot(S2U,cross(nU,p1U))
+                                   +dot(S1U,nU)*dot(S2U,cross(S1U,p1U))
+                                   -5*dot(S1U,nU)**2*dot(S2U,cross(nU,p1U))
+                                   +dot(nU,cross(S1U,S2U))*(+dot(S1U,p1U)
+                                                            -5*dot(S1U,nU)*dot(p1U,nU)))
+                        -3*m1/(2*m2)*(  +dot(S1U,S1U)  *dot(S2U,cross(nU,p2U))
+                                      +2*dot(S1U,nU)   *dot(S2U,cross(S1U,p2U))
+                                      -5*dot(S1U,nU)**2*dot(S2U,cross(nU,p2U)))
+                        -dot(cross(S1U,nU),p2_minus_m2_over_4m1_p1)*(dot(S1U,S1U) - 5*dot(S1U,nU)**2))/(m1**2*r**4)
+        return H_SSS_3PN_pt
     global H_SSS_3PN
-    H_SSS_3PN = (+SHS2015_HSSS_3PN_pt(m1,m2, n12U, S1U, p1U,p2U, q)
-                 +SHS2015_HSSS_3PN_pt(m2,m1, n21U, S2U, p2U,p1U, q))
+    H_SSS_3PN = (+f_H_SSS_3PN_pt(m1,m2, n12U, S1U,S2U, p1U,p2U, r12)
+                 +f_H_SSS_3PN_pt(m2,m1, n21U, S2U,S1U, p2U,p1U, r12))
