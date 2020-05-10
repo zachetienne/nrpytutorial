e54acb084fb78a20c258c5e7974b6d46c237fd60
Zachariah Etienne <zachetie@gmail.com>
2020-05-09 22:34:05 -0400

    NRPyPN: PN-Hamiltonian-Nonspinning: validate all equations

--- NRPyPN/PN_Hamiltonian_NS.py
+++ NRPyPN/PN_Hamiltonian_NS.py
@@ -25,45 +25,42 @@ from outputC import *            # NRPy+: Core C code output module
 import indexedexp as ixp         # NRPy+: Symbolic indexed expression (e.g., tensors, vectors, etc.) support
 from NRPyPN_shortcuts import *   # NRPyPN: shortcuts for e.g., vector operations
 
-def f_H_Newt__H_NS_1PN__H_NS_2PN(m1,m2, pU, nU, q):
-    mu = m1*m2/(m1+m2)
-    PU = ixp.zerorank1()
+def f_H_Newt__H_NS_1PN__H_NS_2PN(m1,m2, PU, nU, q):
+    mu  = m1*m2 / (m1+m2)
+    eta = m1*m2 / (m1+m2)**2
+    pU = ixp.zerorank1()
     for i in range(3):
-        PU[i] = pU[i]/mu
-    eta = m1*m2/(m1+m2)**2
-    P_dot_P = dot(PU,PU)
-    n_dot_P = dot(nU,PU)
+        pU[i] = PU[i]/mu
 
-    # H_{\rm Newt} = \frac{p^i p^i}{2} - \frac{1}{q}
-    global H_Newt,H_NS_1PN,H_NS_2PN
-    H_Newt    = mu*(div(1,2)*P_dot_P - 1/q)
-    H_NS_1PN  = mu*(div(1,8)*(3*eta-1)*P_dot_P**2 - \
-                 div(1,2)*((3+eta)*P_dot_P + eta*n_dot_P**2)/q + 1/(2*q**2))
-    H_NS_2PN  = mu*(div(1,16)*(1 - 5*eta + 5*eta**2)*P_dot_P**3 +
-                 div(1,8)*((5 - 20*eta - 3*eta**2)*P_dot_P**2 
-                           - 2*eta**2*n_dot_P**2*P_dot_P - 3*eta**2*n_dot_P**4)/q +
-                 div(1,2)*((5 + 8*eta)*P_dot_P + 3*eta*n_dot_P**2)/q**2 -
-                 div(1,4)*(1 + 3*eta)/q**3)
+    global H_Newt, H_NS_1PN, H_NS_2PN
+    H_Newt   = mu*(+div(1,2)*dot(pU,pU) - 1/q)
 
-def f_H_NS_3PN(m1,m2, pU, nU, q):
-    mu = m1*m2/(m1+m2)
-    PU = ixp.zerorank1()
+    H_NS_1PN = mu*(+div(1,8)*(3*eta-1)*dot(pU,pU)**2
+                   -div(1,2)*((3+eta)*dot(pU,pU) + eta*dot(nU,pU)**2)/q
+                   +div(1,2)/q**2)
+
+    H_NS_2PN = mu*(+div(1,16)*(1 -  5*eta + 5*eta**2)*dot(pU,pU)**3
+                   +div(1,8)*(+(5 - 20*eta - 3*eta**2)*dot(pU,pU)**2
+                              -2*eta**2*dot(nU,pU)**2*dot(pU,pU)
+                              -3*eta**2*dot(nU,pU)**4)/q
+                   +div(1,2)*((5+8*eta)*dot(pU,pU) + 3*eta*dot(nU,pU)**2)/q**2
+                   -div(1,4)*(1+3*eta)/q**3)
+
+def f_H_NS_3PN(m1,m2, PU, nU, q):
+    mu  = m1*m2 / (m1+m2)
+    eta = m1*m2 / (m1+m2)**2
+    pU = ixp.zerorank1()
     for i in range(3):
-        PU[i] = pU[i]/mu
-    eta = m1*m2/(m1+m2)**2
-    P_dot_P = dot(PU,PU)
-    n_dot_P = dot(nU,PU)
+        pU[i] = PU[i]/mu
 
     global H_NS_3PN
-    # The following is simply by-hand search/replaced from the above LaTeX to minimize error
-    H_NS_3PN = \
-    mu*( div(1,128)*(-5+35*eta-70*eta**2+35*eta**3)*P_dot_P**4 +
-         div(1,16)* ( (-7+42*eta-53*eta**2-5*eta**3)*P_dot_P**3
-                     +(2-3*eta)*eta**2*n_dot_P**2*P_dot_P**2 +
-                     +3*(1-eta)*eta**2*n_dot_P**4*P_dot_P - 5*eta**3*n_dot_P**6 )/(q) +
- (  div(1,16)*(-27+136*eta+109*eta**2)*P_dot_P**2
- + div(1,16)*(17+30*eta)*eta*n_dot_P**2*P_dot_P + div(1,12)*(5+43*eta)*eta*n_dot_P**4)/(q**2) +
- ( ( -div(25,8) + (div(1,64)*sp.pi**2-div(335,48))*eta 
- - div(23,8)*eta**2 )*P_dot_P
- + (-div(85,16)-div(3,64)*sp.pi**2-div(7,4)*eta)*eta*n_dot_P**2)/(q**3) +
- ( div(1,8) + (div(109,12)-div(21,32)*sp.pi**2)*eta)/(q**4) )
+    H_NS_3PN = mu*(+div(1,128)*(-5 + 35*eta - 70*eta**2 + 35*eta**3)*dot(pU,pU)**4
+                   +div(1, 16)*(+(-7 + 42*eta - 53*eta**2 -  5*eta**3)*dot(pU,pU)**3
+                                +(2-3*eta)*eta**2*dot(nU,pU)**2*dot(pU,pU)**2
+                                +3*(1-eta)*eta**2*dot(nU,pU)**4*dot(pU,pU) - 5*eta**3*dot(nU,pU)**6)/q
+                   +(+div(1,16)*(-27 + 136*eta + 109*eta**2)*dot(pU,pU)**2
+                     +div(1,16)*(+17 +  30*eta)*eta*dot(nU,pU)**2*dot(pU,pU)
+                     +div(1,12)*(+ 5 +  43*eta)*eta*dot(nU,pU)**4)/q**2
+                   +(+(-div(25, 8) + (div(1,64)*sp.pi**2 - div(335,48))*eta - div(23,8)*eta**2)*dot(pU,pU)
+                     +(-div(85,16) - div(3,64)*sp.pi**2 - div(7,4)*eta)*eta*dot(nU,pU)**2)/q**3
+                   +(+div(1,8)+(div(109,12) - div(21,32)*sp.pi**2)*eta)/q**4)
