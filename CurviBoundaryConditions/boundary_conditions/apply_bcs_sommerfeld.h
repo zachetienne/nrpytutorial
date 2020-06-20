5cc752c4660d95e320bcb777a48a76c8399c6061
Gonçalo Andrade <goncalo.j.c.andrade@tecnico.ulisboa.pt>
2020-06-20 19:02:39 +0100

    apply_bcs_sommerfeld.h: working version of Sommerfeld BCs in Spherical Coordinates

--- CurviBoundaryConditions/boundary_conditions/apply_bcs_sommerfeld.h
+++ CurviBoundaryConditions/boundary_conditions/apply_bcs_sommerfeld.h
@@ -169,67 +169,81 @@ void apply_bcs_sommerfeld(const paramstruct *restrict params,REAL *restrict xx[3
         } // END for(int which_gz = 0; which_gz < NGHOSTS; which_gz++)
     } // END for(int which_gf=0;which_gf<NUM_GFS;which_gf++)
   }// END if coord = Cartesian
- /* else {
-    #pragma omp parallel for
-        for(int which_gf=0;which_gf<NUM_GFS;which_gf++) {
-          REAL var_at_infinity = evolgf_at_inf[which_gf];
-          REAL radpower = evolgf_radpower[which_gf];
-          REAL char_speed = evolgf_speed[which_gf];
+  /* else if (strcmp(coord, "Spherical") == 0) 
+  {
+#pragma omp parallel for
+    for (int which_gf = 0; which_gf < NUM_GFS; which_gf++)
+    {
+      REAL var_at_infinity = evolgf_at_inf[which_gf];
+      REAL radpower = evolgf_radpower[which_gf];
+      REAL char_speed = evolgf_speed[which_gf];
 
-            for(int which_gz = 0; which_gz < NGHOSTS; which_gz++) {
-                for(int pt=0;pt<bcstruct->num_ob_gz_pts[which_gz];pt++) {
-                    int i0 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i0;
-                    int i1 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i1;
-                    int i2 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i2;
-                    // int8_t FACEX0 = bcstruct->outer[which_gz][pt].FACEi0;
-                    // int8_t FACEX1 = bcstruct->outer[which_gz][pt].FACEi1;
-                    // int8_t FACEX2 = bcstruct->outer[which_gz][pt].FACEi2;
+      for (int which_gz = 0; which_gz < NGHOSTS; which_gz++)
+      {
+        for (int pt = 0; pt < bcstruct->num_ob_gz_pts[which_gz]; pt++)
+        {
+          int i0 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i0;
+          int i1 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i1;
+          int i2 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i2;
+          // int8_t FACEX0 = bcstruct->outer[which_gz][pt].FACEi0;
+          // int8_t FACEX1 = bcstruct->outer[which_gz][pt].FACEi1;
+          // int8_t FACEX2 = bcstruct->outer[which_gz][pt].FACEi2;
 
-                    REAL invr = 1./(xx[0][i0]);
-                    REAL dfdr = 0.;
+          REAL invr = 1. / (xx[0][i0]);
+          REAL dfdr = 0.;
 
-                    // On a +x or -x face, do up/down winding as appropriate:
-                    dfdr = (-3*gfs[IDX4S(which_gf,i0,  i1,i2)]
-                            +4*gfs[IDX4S(which_gf,i0+1,i1,i2)]
-                            -1*gfs[IDX4S(which_gf,i0+2,i1,i2)])*invdx0*0.5;
+          // Backwards finite difference stencil
+          dfdr = (3 * gfs[IDX4S(which_gf, i0, i1, i2)] - 4 * gfs[IDX4S(which_gf, i0 - 1, i1, i2)] + 1 * gfs[IDX4S(which_gf, i0 - 2, i1, i2)]) * invdx0 * 0.5;
 
-                    REAL source_rhs = -char_speed*(dfdr + invr*(gfs[IDX4S(which_gf,i0,i1,i2)] - var_at_infinity));
-                    rhs_gfs[IDX4S(which_gf,i0,i1,i2)] = source_rhs;
+          REAL source_rhs = -char_speed * (dfdr + invr * (gfs[IDX4S(which_gf, i0, i1, i2)] - var_at_infinity));
+          rhs_gfs[IDX4S(which_gf, i0, i1, i2)] = source_rhs;
 
-                     /////////For radial falloff and the extrapolated h'(t) term////////
-                    if (radpower > 0) {
+          /////////For radial falloff and the extrapolated h'(t) term////////
+          if (radpower > 0)
+          {
 
-                      int ip0 = i0+1;
+            int ip0 = i0 - 1; // Move towards the interior of the grid
 
-                      REAL invrp = 1./(xx[0][ip0]);
-                      REAL dfdr = 0.;
+            REAL invrp = 1. / (xx[0][ip0]);
+            REAL dfdr = 0.;
 
-                      dfdr = (-3*gfs[IDX4S(which_gf,ip0  ,i1,i2)]
-                              +4*gfs[IDX4S(which_gf,ip0+1,i1,i2)]
-                              -1*gfs[IDX4S(which_gf,ip0+2,i1,i2)])*invdx0*0.5;
+            // Use centered finite difference stencils if possible, otherwise use backwards derivatives
+            if (ip0 + NGHOSTS >= Nxx_plus_2NGHOSTS2)
+            {
+              // Backwards derivative - notice the sign change
+              dfdr = (3 * gfs[IDX4S(which_gf, ip0, i1, i2)] - 4 * gfs[IDX4S(which_gf, ip0 - 1, i1, i2)] + 1 * gfs[IDX4S(which_gf, ip0 - 2, i1, i2)]) * invdx0 * 0.5;
+            }
+            else
+            {
+              // Standard centered derivative
+              dfdr = (gfs[IDX4S(which_gf, ip0 + 1, i1, i2)] - gfs[IDX4S(which_gf, ip0 - 1, i1, i2)]) * invrp * 0.5;
+            }
 
-
-                      REAL extrap_rhs = char_speed*(dfdr + invrp*(gfs[IDX4S(which_gf,ip0,i1,i2)] - var_at_infinity));
-                      REAL aux = rhs_gfs[IDX4S(which_gf,ip0,i1,i2)] + extrap_rhs;
-                      rhs_gfs[IDX4S(which_gf,i0,i1,i2)] += aux*pow(xx[0][ip0]*invr,radpower);
-                    }
-                }// END for(int pt=0;pt<num_ob_gz_pts[which_gz];pt++)
-                // Then apply INNER (parity) boundary conditions:
-            for(int pt=0;pt<bcstruct->num_ib_gz_pts[which_gz];pt++) {
-                const int i0dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i0;
-                const int i1dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i1;
-                const int i2dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i2;
-                const int i0src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i0;
-                const int i1src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i1;
-                const int i2src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i2;
-                const int8_t *prty= bcstruct->inner[which_gz][pt].parity;
-//                printf("%d\n",bcstruct->inner_bc_parity[which_gz][pt].parity[gfs_parity[which_gf]]);
-                gfs[IDX4S(which_gf,i0dest,i1dest,i2dest)] =
-                        bcstruct->inner[which_gz][pt].parity[gfs_parity[which_gf]] * gfs[IDX4S(which_gf, i0src,i1src,i2src)];
-            }// END for(int pt=0;pt<num_ib_gz_pts[which_gz];pt++)
-        } // END for(int which_gz = 0; which_gz < NGHOSTS; which_gz++)
-    } // END for(int which_gf=0;which_gf<NUM_GFS;which_gf++)
-  }*/ else {
+            // Same as before
+            REAL extrap_rhs = char_speed * (dfdr + invrp * (gfs[IDX4S(which_gf, ip0, i1, i2)] - var_at_infinity));
+            REAL aux = rhs_gfs[IDX4S(which_gf, ip0, i1, i2)] + extrap_rhs;
+            rhs_gfs[IDX4S(which_gf, i0, i1, i2)] += aux * pow(xx[0][ip0] * invr, radpower);
+          }
+        } // END for(int pt=0;pt<num_ob_gz_pts[which_gz];pt++)
+          // Then apply INNER (parity) boundary conditions:
+        for (int pt = 0; pt < bcstruct->num_ib_gz_pts[which_gz]; pt++)
+        {
+          const int i0dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i0;
+          const int i1dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i1;
+          const int i2dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i2;
+          const int i0src = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i0;
+          const int i1src = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i1;
+          const int i2src = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i2;
+          const int8_t *prty = bcstruct->inner[which_gz][pt].parity;
+          //                printf("%d\n",bcstruct->inner_bc_parity[which_gz][pt].parity[gfs_parity[which_gf]]);
+          gfs[IDX4S(which_gf, i0dest, i1dest, i2dest)] =
+              bcstruct->inner[which_gz][pt].parity[gfs_parity[which_gf]] * gfs[IDX4S(which_gf, i0src, i1src, i2src)];
+        } // END for(int pt=0;pt<num_ib_gz_pts[which_gz];pt++)
+      }   // END for(int which_gz = 0; which_gz < NGHOSTS; which_gz++)
+    }     // END for(int which_gf=0;which_gf<NUM_GFS;which_gf++)
+  } */
+  else
+  {
     printf("ERROR: Sommerfeld boundary conditions are currently only enabled for Cartesian coordinates.\n");
     exit(1);
   } // END coord != Cartesian
