75373c4ceb5386a36b9eb3046ba01ea09ef82b0f
penelson16 <penelson@mix.wvu.edu>
2020-07-09 00:53:59 -0400

    GiRaFFE_NRPy: Several new notebooks and modules for handwritten C codes.

---
+++ in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_Afield_flux_handwritten.py
@@ -0,0 +1,168 @@
+# Step 0: Add NRPy's directory to the path
+# https://stackoverflow.com/questions/16780014/import-file-from-parent-directory
+import os,sys
+nrpy_dir_path = os.path.join("..")
+if nrpy_dir_path not in sys.path:
+    sys.path.append(nrpy_dir_path)
+
+import cmdline_helper as cmd     # NRPy+: Multi-platform Python command-line interface
+Ccodesdir = "GiRaFFE_standalone_Ccodes/RHSs"
+cmd.mkdir(os.path.join(Ccodesdir))
+
+def GiRaFFE_NRPy_Afield_flux_handwritten(Ccodesdir):
+    cmd.mkdir(Ccodesdir)
+    # Write out the code to a file.
+    with open(os.path.join(Ccodesdir,"calculate_E_field_flat_all_in_one.h"),"w") as file:
+        file.write("""REAL HLLE_solve(REAL F0B1_r, REAL F0B1_l, REAL U_r, REAL U_l) {
+  // Eq. 3.15 of https://epubs.siam.org/doi/abs/10.1137/1025002?journalCode=siread
+  // F_HLLE = (c_min F_R + c_max F_L - c_min c_max (U_R-U_L)) / (c_min + c_max)
+  return 0.5*(F0B1_r+F0B1_l-(U_r-U_l));
+  // FIXME: Curved space implementation!
+}
+
+/*
+Calculate the electric flux on both faces in the input direction.
+The input count is an integer that is either 0 or 1. If it is 0, this implies
+that the components are input in order of a backwards permutation  and the final
+results will need to be multiplied by -1.0. If it is 1, then the permutation is fowards.
+ */
+void calculate_E_field_flat_all_in_one(const paramstruct *params,
+                                       const REAL *Vr0,const REAL *Vr1,
+                                       const REAL *Vl0,const REAL *Vl1,
+                                       const REAL *Br0,const REAL *Br1,
+                                       const REAL *Bl0,const REAL *Bl1,
+                                       const REAL *Brflux_dirn,
+                                       const REAL *Blflux_dirn,
+                                       REAL *A2_rhs,const REAL SIGN,const int flux_dirn) {
+    // FIXME: include metric functions!
+    // This function is written to be generic and compute the contribution for all three AD RHSs.
+    // However, for convenience, the notation used in the function itself is for the contribution
+    // to AD2, specifically the [F_HLL^x(B^y)]_z term, with reconstructions in the x direction. This
+    // corresponds to flux_dirn=0 and count=1 (which corresponds to SIGN=+1.0).
+    // Thus, Az(i,j,k) += 0.25 ( [F_HLL^x(B^y)]_z(i+1/2,j,k)+[F_HLL^x(B^y)]_z(i-1/2,j,k)) are solved here.
+    // The other terms are computed by cyclically permuting the indices when calling this function.
+#include "GiRaFFE_standalone_Ccodes/set_Cparameters.h"
+
+#pragma omp parallel for
+    for(int i2=NGHOSTS; i2<NGHOSTS+Nxx2; i2++) {
+        for(int i1=NGHOSTS; i1<NGHOSTS+Nxx1; i1++) {
+            for(int i0=NGHOSTS; i0<NGHOSTS+Nxx0; i0++) {
+                // First, we set the index from which we will read memory. indexp1 is incremented by
+                // one point in the direction of reconstruction. These correspond to the faces at at
+                // i-1/2 and i+1/2, respectively.
+
+                // Now, we read in memory. We need the x and y components of velocity and magnetic field on both
+                // the left and right sides of the interface at *both* faces.
+                // Here, the point (i0,i1,i2) corresponds to the point (i-1/2,j,k)
+                const int index            = IDX3S(i0,i1,i2);
+                const double Valenciav_rU0 = Vr0[index];
+                const double Valenciav_rU1 = Vr1[index];
+                const double B_rU0         = Br0[index];
+                const double B_rU1         = Br1[index];
+                const double B_rflux_dirn  = Brflux_dirn[index];
+                const double Valenciav_lU0 = Vl0[index];
+                const double Valenciav_lU1 = Vl1[index];
+                const double B_lU0         = Bl0[index];
+                const double B_lU1         = Bl1[index];
+                const double B_lflux_dirn  = Blflux_dirn[index];
+
+                // *******************************
+                // REPEAT ABOVE, but at i+1, which corresponds to point (i+1/2,j,k)
+                //     Recall that the documentation here assumes flux_dirn==0, but the
+                //     algorithm is generalized so that any flux_dirn or velocity/magnetic
+                //     field component can be computed via permuting the inputs into this
+                //     function.
+                const int indexp1             = IDX3S(i0+(flux_dirn==0),i1+(flux_dirn==1),i2+(flux_dirn==2));
+                const double Valenciav_rU0_p1 = Vr0[indexp1];
+                const double Valenciav_rU1_p1 = Vr1[indexp1];
+                const double B_rU0_p1         = Br0[indexp1];
+                const double B_rU1_p1         = Br1[indexp1];
+                const double B_rflux_dirn_p1  = Brflux_dirn[indexp1];
+                const double Valenciav_lU0_p1 = Vl0[indexp1];
+                const double Valenciav_lU1_p1 = Vl1[indexp1];
+                const double B_lU0_p1         = Bl0[indexp1];
+                const double B_lU1_p1         = Bl1[indexp1];
+                const double B_lflux_dirn_p1  = Blflux_dirn[indexp1];
+                // *******************************
+
+                // DEBUGGING:
+//                 if(flux_dirn==0 && SIGN>0 && i1==Nxx_plus_2NGHOSTS1/2 && i2==Nxx_plus_2NGHOSTS2/2) {
+//                     printf("index=%d & indexp1=%d\n",index,indexp1);
+//                 }
+
+                // Since we are computing A_z, the relevant equation here is:
+                // -E_z(x_i,y_j,z_k) = 0.25 ( [F_HLL^x(B^y)]_z(i+1/2,j,k)+[F_HLL^x(B^y)]_z(i-1/2,j,k)
+                //                           -[F_HLL^y(B^x)]_z(i,j+1/2,k)-[F_HLL^y(B^x)]_z(i,j-1/2,k) )
+                // We will construct the above sum one half at a time, first with SIGN=+1, which
+                // corresponds to flux_dirn = 0, count=1, and
+                //  takes care of the terms:
+                //  [F_HLL^x(B^y)]_z(i+1/2,j,k)+[F_HLL^x(B^y)]_z(i-1/2,j,k)
+
+                // ( Note that we will repeat the above with flux_dirn = 1, count = 0, with SIGN=-1
+                //   AND with the input components switched (x->y,y->x) so that we get the term
+                // -[F_HLL^y(B^x)]_z(i,j+1/2,k)-[F_HLL^y(B^x)]_z(i,j-1/2,k)
+                // thus completing the above sum. )
+
+                // Here, [F_HLL^i(B^j)]_k = (v^i B^j - v^j B^i) in general.
+
+                // Calculate the flux vector on each face for each component of the E-field:
+                // The F(B) terms are as Eq. 6 in Giacomazzo: https://arxiv.org/pdf/1009.2468.pdf
+                // [F^i(B^j)]_k = \sqrt{\gamma} (v^i B^j - v^j B^i)
+                // Therefore since we want [F_HLL^x(B^y)]_z,
+                // we will code     (v^x           B^y   - v^y           B^x) on both left and right faces.
+                const REAL F0B1_r = (Valenciav_rU0*B_rU1 - Valenciav_rU1*B_rU0);
+                const REAL F0B1_l = (Valenciav_lU0*B_lU1 - Valenciav_lU1*B_lU0);
+
+                // ZACH SAYS: Make sure the below is documented!
+                // Compute the state vector for this flux direction
+                // We must also multiply by sign so that we use the positive for the forward permutation
+                // and negative for the backwards permutation. For Az, that means that we add +By and -Bx,
+                // exactly as is done in the original GiRaFFE's A_i_rhs_no_gauge_terms.C, in line with
+                // Del Zanna, 2003 [https://arxiv.org/pdf/astro-ph/0210618.pdf], Eq. 44
+                const REAL U_r = B_rflux_dirn; //B_rU0;
+                const REAL U_l = B_lflux_dirn;
+
+                // Basic HLLE solver:
+                const REAL FHLL_0B1 = HLLE_solve(F0B1_r, F0B1_l, U_r, U_l);
+
+                // ************************************
+                // ************************************
+                // REPEAT ABOVE, but at point i+1
+                // Calculate the flux vector on each face for each component of the E-field:
+                const REAL F0B1_r_p1 = (Valenciav_rU0_p1*B_rU1_p1 - Valenciav_rU1_p1*B_rU0_p1);
+                const REAL F0B1_l_p1 = (Valenciav_lU0_p1*B_lU1_p1 - Valenciav_lU1_p1*B_lU0_p1);
+
+                // Compute the state vector for this flux direction
+                const REAL U_r_p1 = B_rflux_dirn_p1;
+                const REAL U_l_p1 = B_lflux_dirn_p1;
+                //const REAL U_r_p1 = B_rU1_p1;
+                //const REAL U_l_p1 = B_lU1_p1;
+                // Basic HLLE solver, but at the next point:
+                const REAL FHLL_0B1p1 = HLLE_solve(F0B1_r_p1, F0B1_l_p1, U_r_p1, U_l_p1);
+                // ************************************
+                // ************************************
+
+
+                // With the Riemann problem solved, we add the contributions to the RHSs:
+                // -E_z(x_i,y_j,z_k) &= 0.25 ( [F_HLL^x(B^y)]_z(i+1/2,j,k)+[F_HLL^x(B^y)]_z(i-1/2,j,k)
+                //                            -[F_HLL^y(B^x)]_z(i,j+1/2,k)-[F_HLL^y(B^x)]_z(i,j-1/2,k) )
+                // (Eq. 11 in https://arxiv.org/pdf/1009.2468.pdf)
+                // This code, as written, solves the first two terms for flux_dirn=0. Calling this function for count=1
+                // flips x for y to solve the latter two, switching to SIGN=-1 as well.
+
+                // Here, we finally add together the output of the HLLE solver at i-1/2 and i+1/2
+                // We also multiply by the SIGN dictated by the order of the input vectors and divide by 4.
+                A2_rhs[index] += SIGN*0.25*(FHLL_0B1 + FHLL_0B1p1);
+                // flux dirn = 0 ===================>   i-1/2       i+1/2
+                //               Eq 11 in Giacomazzo:
+                //               -FxBy(avg over i-1/2 and i+1/2) + FyBx(avg over j-1/2 and j+1/2)
+                //               Eq 6 in Giacomazzo:
+                //               FxBy = vxBy - vyBx
+                //             ->
+                //               FHLL_0B1 = vyBx - vxBy
+
+            } // END LOOP: for(int i0=NGHOSTS; i0<NGHOSTS+Nxx0; i0++)
+        } // END LOOP: for(int i1=NGHOSTS; i1<NGHOSTS+Nxx1; i1++)
+    } // END LOOP: for(int i2=NGHOSTS; i2<NGHOSTS+Nxx2; i2++)
+}
+""")
---
+++ in_progress/GiRaFFE_NRPy/GiRaFFE_NRPy_staggered_Afield_flux.py
@@ -0,0 +1,118 @@
+# Step 0: Add NRPy's directory to the path
+# https://stackoverflow.com/questions/16780014/import-file-from-parent-directory
+import os,sys
+nrpy_dir_path = os.path.join("..")
+if nrpy_dir_path not in sys.path:
+    sys.path.append(nrpy_dir_path)
+
+import cmdline_helper as cmd     # NRPy+: Multi-platform Python command-line interface
+Ccodesdir = "GiRaFFE_standalone_Ccodes/RHSs"
+cmd.mkdir(os.path.join(Ccodesdir))
+
+def GiRaFFE_NRPy_Afield_flux(Ccodesdir):
+    cmd.mkdir(Ccodesdir)
+    # Write out the code to a file.
+    with open(os.path.join(Ccodesdir,"calculate_E_field_flat_all_in_one.h"),"w") as file:
+        file.write("""/* Compute the part of A_i_rhs that excludes the gauge terms. I.e., we set
+ *   A_i_rhs = \partial_t A_i = \psi^{6} (v^z B^x - v^x B^z)   here.
+ */
+static void A_i_rhs_no_gauge_terms(const int A_dirn,const paramstruct *params,gf_and_gz_struct *out_prims_r,gf_and_gz_struct *out_prims_l,
+                                   REAL *psi6_pointer,REAL *cmax_1,REAL *cmin_1,REAL *cmax_2,REAL *cmin_2, REAL *A3_rhs) {
+  #include "../set_Cparameters.h"
+
+  // If A_dirn=1, then v1_offset=1 (v1=VY) and v2_offset=2 (v2=VZ)
+  // If A_dirn=2, then v1_offset=2 (v1=VZ) and v2_offset=0 (v2=VX)
+  // If A_dirn=3, then v1_offset=0 (v1=VX) and v2_offset=1 (v2=VY)
+  const int v1_offset  = ((A_dirn-1)+1)%3,        v2_offset = ((A_dirn-1)+2)%3;
+
+  const REAL *v1rr=out_prims_r[VXR+v1_offset].gf, *v2rr=out_prims_r[VXR+v2_offset].gf;
+  const REAL *v1rl=out_prims_l[VXR+v1_offset].gf, *v2rl=out_prims_l[VXR+v2_offset].gf;
+  const REAL *v1lr=out_prims_r[VXL+v1_offset].gf, *v2lr=out_prims_r[VXL+v2_offset].gf;
+  const REAL *v1ll=out_prims_l[VXL+v1_offset].gf, *v2ll=out_prims_l[VXL+v2_offset].gf;
+
+  const REAL *B1r=out_prims_r[BX_STAGGER+v1_offset].gf, *B1l=out_prims_l[BX_STAGGER+v1_offset].gf;
+  const REAL *B2r=out_prims_r[BX_STAGGER+v2_offset].gf, *B2l=out_prims_l[BX_STAGGER+v2_offset].gf;
+
+  /**** V DEPENDENCIES ****/
+  /* In the case of Ax_rhs, we need v{y,z}{r,l} at (i,j+1/2,k+1/2).
+   *    However, v{y,z}{r,l}{r,l} are defined at (i,j-1/2,k-1/2), so
+   *    v{y,z}{r,l} at (i,j+1/2,k+1/2) is stored at v{y,z}{r,l}{r,l}(i,j+1,k+1).
+   * In the case of Ay_rhs, we need v{x,z}{r,l} at (i+1/2,j,k+1/2).
+   *    However, v{x,z}{r,l}{r,l} are defined at (i-1/2,j,k-1/2), so
+   *    v{x,z}{r,l} at (i+1/2,j,k+1/2) is stored at v{x,z}{r,l}{r,l}(i+1,j,k+1).
+   * In the case of Az_rhs, we need v{x,y}{r,l} at (i+1/2,j+1/2,k).
+   *    However, v{x,y}{r,l}{r,l} are defined at (i-1/2,j-1/2,k), so
+   *    v{x,y}{r,l} at (i+1/2,j+1/2,k) is stored at v{x,y}{r,l}{r,l}(i+1,j+1,k). */
+  static const int vs_ijk_offset[4][3] = { {0,0,0} , {0,1,1} , {1,0,1} , {1,1,0} }; // Note that vs_ijk_offset[0] is UNUSED; we choose a 1-offset for convenience.
+
+  /**** B DEPENDENCIES ****/
+  /* In the case of Ax_rhs, we need B{y,z}{r,l} at (i,j+1/2,k+1/2).
+   *    However, By_stagger{r,l} is defined at (i,j+1/2,k-1/2), and
+   *             Bz_stagger{r,l} is defined at (i,j-1/2,k+1/2), so
+   *             By_stagger{r,l} at (i,j+1/2,k+1/2) is stored at By_stagger{r,l}(i,j,k+1), and
+   *             Bz_stagger{r,l} at (i,j+1/2,k+1/2) is stored at Bz_stagger{r,l}(i,j+1,k).
+   * In the case of Ay_rhs, we need B{z,x}_stagger{r,l} at (i+1/2,j,k+1/2).
+   *    However, Bz_stagger{r,l} is defined at (i-1/2,j,k+1/2), and
+   *             Bx_stagger{r,l} is defined at (i+1/2,j,k-1/2), so
+   *             Bz_stagger{r,l} at (i+1/2,j,k+1/2) is stored at Bz_stagger{r,l}(i+1,j,k), and
+   *             Bx_stagger{r,l} at (i+1/2,j,k+1/2) is stored at Bx_stagger{r,l}(i,j,k+1).
+   * In the case of Az_rhs, we need B{x,y}_stagger{r,l} at (i+1/2,j+1/2,k).
+   *    However, Bx_stagger{r,l} is defined at (i+1/2,j-1/2,k), and
+   *             By_stagger{r,l} is defined at (i-1/2,j+1/2,k), so
+   *             Bx_stagger{r,l} at (i+1/2,j+1/2,k) is stored at Bx_stagger{r,l}(i,j+1,k), and
+   *             By_stagger{r,l} at (i+1/2,j+1/2,k) is stored at By_stagger{r,l}(i+1,j,k).
+   */
+  static const int B1_ijk_offset[4][3] = { {0,0,0} , {0,0,1} , {1,0,0} , {0,1,0} }; // Note that B1_ijk_offset[0] is UNUSED; we choose a 1-offset for convenience.
+  static const int B2_ijk_offset[4][3] = { {0,0,0} , {0,1,0} , {0,0,1} , {1,0,0} }; // Note that B2_ijk_offset[0] is UNUSED; we choose a 1-offset for convenience.
+
+#pragma omp parallel for
+  for(int k=NGHOSTS;k<Nxx_plus_2NGHOSTS2-NGHOSTS;k++) for(int j=NGHOSTS;j<Nxx_plus_2NGHOSTS1-NGHOSTS;j++) for(int i=NGHOSTS;i<Nxx_plus_2NGHOSTS0-NGHOSTS;i++) {
+        const int index=IDX3S(i,j,k);
+        // The following lines set the indices appropriately. See justification in exorbitant comments above.
+        const int index_v =IDX3S(i+vs_ijk_offset[A_dirn][0],j+vs_ijk_offset[A_dirn][1],k+vs_ijk_offset[A_dirn][2]);
+        const int index_B1=IDX3S(i+B1_ijk_offset[A_dirn][0],j+B1_ijk_offset[A_dirn][1],k+B1_ijk_offset[A_dirn][2]);
+        const int index_B2=IDX3S(i+B2_ijk_offset[A_dirn][0],j+B2_ijk_offset[A_dirn][1],k+B2_ijk_offset[A_dirn][2]);
+
+        // Stores 1/sqrt(gamma)==exp(6 phi) at (i+1/2,j+1/2,k) for Az, (i+1/2,j,k+1/2) for Ay, and (i,j+1/2,k+1/2) for Az.
+        const REAL psi6_interped=psi6_pointer[index];
+
+        const REAL B1lL = B1l[index_B1];
+        const REAL B1rL = B1r[index_B1];
+        const REAL B2lL = B2l[index_B2];
+        const REAL B2rL = B2r[index_B2];
+
+        const REAL A3_rhs_rr = psi6_interped*(v1rr[index_v]*B2rL - v2rr[index_v]*B1rL);
+        const REAL A3_rhs_rl = psi6_interped*(v1rl[index_v]*B2rL - v2rl[index_v]*B1lL);
+        const REAL A3_rhs_lr = psi6_interped*(v1lr[index_v]*B2lL - v2lr[index_v]*B1rL);
+        const REAL A3_rhs_ll = psi6_interped*(v1ll[index_v]*B2lL - v2ll[index_v]*B1lL);
+
+
+        // All variables for the A_i_rhs computation are now at the appropriate staggered point,
+        //   so it's time to compute the HLL flux!
+
+        // Note that with PPM, cmin and cmax are defined between ijk=3 and ijk<cctk_lsh[]-2 for all directions.
+        const REAL cmax_1L = cmax_1[index_B2];
+        const REAL cmin_1L = cmin_1[index_B2];
+        const REAL cmax_2L = cmax_2[index_B1];
+        const REAL cmin_2L = cmin_2[index_B1];
+
+        const REAL B1tilder_minus_B1tildel = psi6_interped*( B1rL - B1lL );
+        const REAL B2tilder_minus_B2tildel = psi6_interped*( B2rL - B2lL );
+
+        /*---------------------------
+         * Implement 2D HLL flux
+         * [see Del Zanna, Bucciantini & Londrillo A&A 400, 397 (2003), Eq. (44)]
+         *
+         * Note that cmax/cmin (\alpha^{\pm}  as defined in that paper) is at a slightly DIFFERENT
+         * point than that described in the Del Zanna et al paper (e.g., (i+1/2,j,k) instead of
+         * (i+1/2,j+1/2,k) for F3).  Yuk Tung Liu discussed this point with M. Shibata,
+         * who found that the effect is negligible.
+         ---------------------------*/
+        A3_rhs[index] = (cmax_1L*cmax_2L*A3_rhs_ll + cmax_1L*cmin_2L*A3_rhs_lr +
+                         cmin_1L*cmax_2L*A3_rhs_rl + cmin_1L*cmin_2L*A3_rhs_rr)
+          /( (cmax_1L+cmin_1L)*(cmax_2L+cmin_2L) )
+          - cmax_1L*cmin_1L*(B2tilder_minus_B2tildel)/(cmax_1L+cmin_1L)
+          + cmax_2L*cmin_2L*(B1tilder_minus_B1tildel)/(cmax_2L+cmin_2L);
+      }
+}
+""")
