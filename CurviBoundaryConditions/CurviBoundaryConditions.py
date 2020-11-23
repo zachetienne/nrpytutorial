bdfc326b488004008fc0f97894e462ce68235967
Terrence Pierre <terrencepierrej@gmail.com>
2020-11-23 14:16:19 -0500

    Updated CurviBoundaryConditions.py file: Now generates the apply_bcs_sommerfeld.h file, and fully functions in curviliniear coordinates

--- CurviBoundaryConditions/CurviBoundaryConditions.py
+++ CurviBoundaryConditions/CurviBoundaryConditions.py
@@ -43,7 +43,7 @@ def Set_up_CurviBoundaryConditions(Ccodesdir,verbose=True,Cparamspath=os.path.jo
 
 
         elif str(BoundaryCondition) == "Sommerfeld":
-            for file in ["apply_bcs_sommerfeld.h", "BCs_data_structs.h", "bcstruct_freemem.h", "CurviBC_include_Cfunctions.h",
+            for file in ["BCs_data_structs.h", "bcstruct_freemem.h", "CurviBC_include_Cfunctions.h",
                          "driver_bcstruct.h", "set_bcstruct.h", "set_up__bc_gz_map_and_parity_condns.h"]:
                 shutil.copy(os.path.join("CurviBoundaryConditions", "boundary_conditions", file),
                             os.path.join(Ccodesdir))
@@ -53,7 +53,7 @@ def Set_up_CurviBoundaryConditions(Ccodesdir,verbose=True,Cparamspath=os.path.jo
 
 
         elif str(BoundaryCondition) == "QuadraticExtrapolation&Sommerfeld":
-            for file in ["apply_bcs_curvilinear.h","apply_bcs_sommerfeld.h", "BCs_data_structs.h", "bcstruct_freemem.h", "CurviBC_include_Cfunctions.h",
+            for file in ["apply_bcs_curvilinear.h", "BCs_data_structs.h", "bcstruct_freemem.h", "CurviBC_include_Cfunctions.h",
                          "driver_bcstruct.h", "set_bcstruct.h", "set_up__bc_gz_map_and_parity_condns.h"]:
                 shutil.copy(os.path.join("CurviBoundaryConditions", "boundary_conditions", file),
                             os.path.join(Ccodesdir))
@@ -256,7 +256,7 @@ def Set_up_CurviBoundaryConditions(Ccodesdir,verbose=True,Cparamspath=os.path.jo
 # Sommerfeld boundary condition class; generates Sommerfeld parameters and functions to be used in subsequent
 # C code
 # Author: Terrence Pierre Jacques
-class sommerfeld_bc():
+class sommerfeld_boundary_condition_class():
     """
     Class for generating C code to apply Sommerfeld boundary conditions
     """
@@ -264,9 +264,18 @@ class sommerfeld_bc():
     # Set class variable default values
     # radial falloff power n = 3 has been found to yield the best results
     #  - see Tutorial-SommerfeldBoundaryCondition.ipynb Step 2 for details
-    def __init__(self, vars_at_inf_default = 0., vars_radial_falloff_power_default = 3., vars_speed_default = 1.):
+    def __init__(self, fd_order=2, vars_at_inf_default = 0., vars_radial_falloff_power_default = 3., vars_speed_default = 1.):
         evolved_variables_list, auxiliary_variables_list, auxevol_variables_list = \
                                                         gri.gridfunction_lists()
+
+        # set class finite differencing order
+        self.fd_order = fd_order
+
+        NRPy_FD_order = par.parval_from_str("finite_difference::FD_CENTDERIVS_ORDER")
+
+        if NRPy_FD_order < fd_order:
+            print("ERROR: The global central finite differencing order within NRPy+ must be greater than or equal to the Sommerfeld boundary condition's finite differencing order")
+            sys.exit(1)
 
         # Define class dictionaries to store sommerfeld parameters for each EVOL gridfunction
 
@@ -285,7 +294,7 @@ class sommerfeld_bc():
             self.vars_radial_falloff_power[gf.upper() + 'GF'] = vars_radial_falloff_power_default
             self.vars_speed[gf.upper() + 'GF'] = vars_speed_default
 
-    def write_to_sommerfeld_params_file(self, Ccodesdir):
+    def sommerfeld_params(self):
         # Write parameters to C file
 
         # Creating array for EVOL gridfunction values at infinity
@@ -307,16 +316,15 @@ class sommerfeld_bc():
         var_speed_string = var_speed_string[:-2] + "};"
 
         # Writing to values to sommerfeld_params.h file
-        # Note that we also record the coordinate system
-        with open(os.path.join(Ccodesdir,"boundary_conditions/sommerfeld_params.h"),"w") as file:
-            file.write("""
-// Coordinate system
+        out_str = """
+// Sommerfeld EVOL grid function parameters
 const REAL evolgf_at_inf[NUM_EVOL_GFS] = """+var_at_inf_string+"""
 const REAL evolgf_radial_falloff_power[NUM_EVOL_GFS] = """+vars_radial_falloff_power_string+"""
 const REAL evolgf_speed[NUM_EVOL_GFS] = """+var_speed_string+"""
-""")
+"""
+        return out_str
 
-    def write_dfdr_function(self, Ccodesdir, fd_order=2):
+    def dfdr_function(self, fd_order):
         # function to write c code to calculate dfdr term in Sommerfeld boundary condition
 
         # Read what # of dimensions being usded
@@ -351,7 +359,9 @@ const REAL evolgf_speed[NUM_EVOL_GFS] = """+var_speed_string+"""
         contraction = sp.sympify(0)
         for i in range(3):
             contraction += fdD[i]*Jac_dUrfm_dDSphUD[i][0]
-        contraction = sp.simplify(contraction)
+
+        if do_simplify:
+            contraction = sp.simplify(contraction)
 
         r_str_and_contraction_str = outputC([rfm.xxSph[0],contraction],
                  ["*_r","*_partial_i_f"],filename="returnstring",params="includebraces=False")
@@ -364,13 +374,51 @@ const REAL evolgf_speed[NUM_EVOL_GFS] = """+var_speed_string+"""
                     return "(gfs[IDX4S(which_gf,i0,i1+1,i2)]-gfs[IDX4S(which_gf,i0,i1-1,i2)])*0.5"  # Does not include the 1/dy multiplication
                 elif intdirn == 2:
                     return "(gfs[IDX4S(which_gf,i0,i1,i2+1)]-gfs[IDX4S(which_gf,i0,i1,i2-1)])*0.5"  # Does not include the 1/dz multiplication
+            elif fd_order == 4:
+                if intdirn == 0:
+                    return """(-c2*gfs[IDX4S(which_gf,i0+2,i1,i2)]
+                               +c1*gfs[IDX4S(which_gf,i0+1,i1,i2)]
+                               -c1*gfs[IDX4S(which_gf,i0-1,i1,i2)]
+                               +c2*gfs[IDX4S(which_gf,i0-2,i1,i2)])"""  # Does not include the 1/dx multiplication
+                elif intdirn == 1:
+                    return """(-c2*gfs[IDX4S(which_gf,i0,i1+2,i2)]
+                               +c1*gfs[IDX4S(which_gf,i0,i1+1,i2)]
+                               -c1*gfs[IDX4S(which_gf,i0,i1-1,i2)]
+                               +c2*gfs[IDX4S(which_gf,i0,i1-2,i2)])"""  # Does not include the 1/dy multiplication
+                elif intdirn == 2:
+                    return """(-c2*gfs[IDX4S(which_gf,i0,i1,i2+2)]
+                               +c1*gfs[IDX4S(which_gf,i0,i1,i2+1)]
+                               -c1*gfs[IDX4S(which_gf,i0,i1,i2-1)]
+                               +c2*gfs[IDX4S(which_gf,i0,i1,i2-2)])"""  # Does not include the 1/dz multiplication
+            elif fd_order == 6:
+                if intdirn == 0:
+                    return """( c3*gfs[IDX4S(which_gf,i0+3,i1,i2)]
+                               -c2*gfs[IDX4S(which_gf,i0+2,i1,i2)]
+                               +c1*gfs[IDX4S(which_gf,i0+1,i1,i2)]
+                               -c1*gfs[IDX4S(which_gf,i0-1,i1,i2)]
+                               +c2*gfs[IDX4S(which_gf,i0-2,i1,i2)]
+                               -c3*gfs[IDX4S(which_gf,i0-3,i1,i2)])"""  # Does not include the 1/dx multiplication
+                elif intdirn == 1:
+                    return """( c3*gfs[IDX4S(which_gf,i0,i1+3,i2)]
+                               -c2*gfs[IDX4S(which_gf,i0,i1+2,i2)]
+                               +c1*gfs[IDX4S(which_gf,i0,i1+1,i2)]
+                               -c1*gfs[IDX4S(which_gf,i0,i1-1,i2)]
+                               +c2*gfs[IDX4S(which_gf,i0,i1-2,i2)]
+                               -c3*gfs[IDX4S(which_gf,i0,i1-3,i2)])"""  # Does not include the 1/dy multiplication
+                elif intdirn == 2:
+                    return """( c3*gfs[IDX4S(which_gf,i0,i1,i2+3)]
+                               -c2*gfs[IDX4S(which_gf,i0,i1,i2+2)]
+                               +c1*gfs[IDX4S(which_gf,i0,i1,i2+1)]
+                               -c1*gfs[IDX4S(which_gf,i0,i1,i2-1)]
+                               +c2*gfs[IDX4S(which_gf,i0,i1,i2-2)]
+                               -c3*gfs[IDX4S(which_gf,i0,i1,i2-3)])"""  # Does not include the 1/dz multiplication
 
         def output_dfdx(intdirn, fd_order):
             dirn = str(intdirn)
             dirnp1 = str((intdirn+1)%3)  # if dirn='0', then we want this to be '1'; '1' then '2'; and '2' then '0'
             dirnp2 = str((intdirn+2)%3)  # if dirn='0', then we want this to be '2'; '1' then '0'; and '2' then '1'
-            if fd_order == 2:
-                return """
+
+            preface = """
 // On a +x"""+dirn+""" or -x"""+dirn+""" face, do up/down winding as appropriate:
 if(abs(FACEXi["""+dirn+"""])==1 || i"""+dirn+"""+NGHOSTS >= Nxx_plus_2NGHOSTS"""+dirn+""" || i"""+dirn+"""-NGHOSTS <= 0) {
     int8_t SHIFTSTENCIL"""+dirn+""" = FACEXi["""+dirn+"""];
@@ -378,6 +426,9 @@ if(abs(FACEXi["""+dirn+"""])==1 || i"""+dirn+"""+NGHOSTS >= Nxx_plus_2NGHOSTS"""
     if(i"""+dirn+"""-NGHOSTS <= 0)                  SHIFTSTENCIL"""+dirn+""" = +1;
     SHIFTSTENCIL"""+dirnp1+""" = 0;
     SHIFTSTENCIL"""+dirnp2+""" = 0;
+"""
+            if fd_order == 2:
+                return preface + """
 
     fdD"""+dirn+"""
         = SHIFTSTENCIL"""+dirn+"""*(-1.5*gfs[IDX4S(which_gf,i0+0*SHIFTSTENCIL0,i1+0*SHIFTSTENCIL1,i2+0*SHIFTSTENCIL2)]
@@ -390,12 +441,47 @@ if(abs(FACEXi["""+dirn+"""])==1 || i"""+dirn+"""+NGHOSTS >= Nxx_plus_2NGHOSTS"""
     fdD"""+dirn+""" = """+gen_central_fd_stencil_str(intdirn, 2)+"""*invdx"""+dirn+""";
 }
 """
+            elif fd_order == 4:
+                return preface + """
+
+    fdD"""+dirn+"""
+        = SHIFTSTENCIL"""+dirn+"""*(u0*gfs[IDX4S(which_gf,i0+0*SHIFTSTENCIL0,i1+0*SHIFTSTENCIL1,i2+0*SHIFTSTENCIL2)]
+                         +u1*gfs[IDX4S(which_gf,i0+1*SHIFTSTENCIL0,i1+1*SHIFTSTENCIL1,i2+1*SHIFTSTENCIL2)]
+                         +u2*gfs[IDX4S(which_gf,i0+2*SHIFTSTENCIL0,i1+2*SHIFTSTENCIL1,i2+2*SHIFTSTENCIL2)]
+                         +u3*gfs[IDX4S(which_gf,i0+3*SHIFTSTENCIL0,i1+3*SHIFTSTENCIL1,i2+3*SHIFTSTENCIL2)]
+                         +u4*gfs[IDX4S(which_gf,i0+4*SHIFTSTENCIL0,i1+4*SHIFTSTENCIL1,i2+4*SHIFTSTENCIL2)]
+                        )*invdx"""+dirn+""";
+
+// Not on a +x"""+dirn+""" or -x"""+dirn+""" face, using centered difference:
+} else {
+    fdD"""+dirn+""" = """+gen_central_fd_stencil_str(intdirn, 4)+"""*invdx"""+dirn+""";
+}
+"""
+            elif fd_order == 6:
+                return preface + """
+
+    fdD"""+dirn+"""
+        = SHIFTSTENCIL"""+dirn+"""*(u0*gfs[IDX4S(which_gf,i0+0*SHIFTSTENCIL0,i1+0*SHIFTSTENCIL1,i2+0*SHIFTSTENCIL2)]
+                         +u1*gfs[IDX4S(which_gf,i0+1*SHIFTSTENCIL0,i1+1*SHIFTSTENCIL1,i2+1*SHIFTSTENCIL2)]
+                         +u2*gfs[IDX4S(which_gf,i0+2*SHIFTSTENCIL0,i1+2*SHIFTSTENCIL1,i2+2*SHIFTSTENCIL2)]
+                         +u3*gfs[IDX4S(which_gf,i0+3*SHIFTSTENCIL0,i1+3*SHIFTSTENCIL1,i2+3*SHIFTSTENCIL2)]
+                         +u4*gfs[IDX4S(which_gf,i0+4*SHIFTSTENCIL0,i1+4*SHIFTSTENCIL1,i2+4*SHIFTSTENCIL2)]
+                         +u5*gfs[IDX4S(which_gf,i0+5*SHIFTSTENCIL0,i1+5*SHIFTSTENCIL1,i2+5*SHIFTSTENCIL2)]
+                         +u6*gfs[IDX4S(which_gf,i0+6*SHIFTSTENCIL0,i1+6*SHIFTSTENCIL1,i2+6*SHIFTSTENCIL2)]
+                        )*invdx"""+dirn+""";
+
+// Not on a +x"""+dirn+""" or -x"""+dirn+""" face, using centered difference:
+} else {
+    fdD"""+dirn+""" = """+gen_central_fd_stencil_str(intdirn, 6)+"""*invdx"""+dirn+""";
+}
+"""
             else:
                 print("Error: fd_order = "+str(fd_order)+" currently unsupported.")
                 sys.exit(1)
 
         contraction_term_func = """
 
+// Function to calculate the radial derivative of a grid function
 void contraction_term(const paramstruct *restrict params, const int which_gf, const REAL *restrict gfs, REAL *restrict xx[3],
            const int8_t FACEXi[3], const int i0, const int i1, const int i2, REAL *restrict _r, REAL *restrict _partial_i_f) {
 
@@ -417,6 +503,37 @@ int8_t SHIFTSTENCIL1;
 int8_t SHIFTSTENCIL2;
 
 """
+        if fd_order == 4:
+            contraction_term_func +="""
+// foward/backward finite difference coefficients
+const REAL u0 =-25./12.;
+const REAL u1 = 4.;
+const REAL u2 = -3.;
+const REAL u3 = 4./3.;
+const REAL u4 = -1./4.;
+
+// central finite difference coefficients
+const REAL c1 = 2./3.;
+const REAL c2 = 1./12.;
+
+"""
+        if fd_order == 6:
+            contraction_term_func +="""
+// foward/backward finite difference coefficients
+const REAL u0 = -49./20.;
+const REAL u1 =  6.;
+const REAL u2 = -15./2.;
+const REAL u3 =  20./3.;
+const REAL u4 = -15./4.;
+const REAL u5 =  6./5.;
+const REAL u6 = -1./6.;
+
+// central finite difference coefficients
+const REAL c1 = 3./4.;
+const REAL c2 = 3./20.;
+const REAL c3 = 1./60;
+
+"""
         for i in range(DIM):
             if "fdD"+str(i) in r_str_and_contraction_str:
                 contraction_term_func += output_dfdx(i, fd_order)
@@ -426,9 +543,108 @@ int8_t SHIFTSTENCIL2;
         contraction_term_func +="""
 } // END contraction_term function
 """
-        with open(os.path.join(Ccodesdir,"boundary_conditions/radial_derivative.h"),"w") as file:
-            file.write(contraction_term_func)
+        return contraction_term_func
 
-    def write_sommerfeld_files(self, Ccodesdir, fd_order=2):
-        self.write_dfdr_function(Ccodesdir, fd_order)
-        self.write_to_sommerfeld_params_file(Ccodesdir)
+    def write_sommerfeld_main_Ccode(self, Ccodesdir):
+        main_Ccode = """
+// Boundary condtion driver routine: Apply BCs to all
+// boundary faces of the 3D numerical domain, filling in the
+// outer boundary ghost zone layers, starting with the innermost
+// layer and working outward.
+"""
+        main_Ccode += self.sommerfeld_params()
+        main_Ccode += self.dfdr_function(self.fd_order)
+
+        main_Ccode += """
+void apply_bcs_sommerfeld(const paramstruct *restrict params, REAL *restrict xx[3],
+                          const bc_struct *restrict bcstruct, const int NUM_GFS,
+                          const int8_t *restrict gfs_parity, REAL *restrict gfs,
+                          REAL *restrict rhs_gfs) {
+
+    #pragma omp parallel for
+        for(int which_gf=0;which_gf<NUM_GFS;which_gf++) {
+          const REAL char_speed             = evolgf_speed[which_gf];
+          const REAL var_at_infinity        = evolgf_at_inf[which_gf];
+          const REAL radial_falloff_power = evolgf_radial_falloff_power[which_gf];
+
+
+          #include "RELATIVE_PATH__set_Cparameters.h" /* Header file containing correct #include for set_Cparameters.h;
+                                                       * accounting for the relative path */
+
+
+            for(int which_gz = 0; which_gz < NGHOSTS; which_gz++) {
+                for(int pt=0;pt<bcstruct->num_ob_gz_pts[which_gz];pt++) {
+                    const int i0 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i0;
+                    const int i1 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i1;
+                    const int i2 = bcstruct->outer[which_gz][pt].outer_bc_dest_pt.i2;
+                    const int8_t FACEX0 = bcstruct->outer[which_gz][pt].FACEi0;
+                    const int8_t FACEX1 = bcstruct->outer[which_gz][pt].FACEi1;
+                    const int8_t FACEX2 = bcstruct->outer[which_gz][pt].FACEi2;
+
+                    const int8_t FACEXi[3] = {FACEX0, FACEX1, FACEX2};
+
+                    // Initialize derivatives to crazy values, to ensure that
+                    //   we will notice in case they aren't set properly.
+                    REAL r = 1e100;
+                    REAL partial_i_f = 1e100;
+
+                    contraction_term(params, which_gf, gfs, xx, FACEXi, i0, i1, i2, &r, &partial_i_f);
+
+                    const REAL invr = 1./r;
+
+                    const REAL source_rhs = -char_speed*(partial_i_f + invr*(gfs[IDX4S(which_gf,i0,i1,i2)] - var_at_infinity));
+                    rhs_gfs[IDX4S(which_gf,i0,i1,i2)] = source_rhs;
+
+                    /************* For radial falloff and the extrapolated k term *************/
+                    if (radial_falloff_power > 0) {
+
+                      // Move one point away from gz point to compare pure advection to df/dt|interior
+
+                      const int i0_offset = i0+FACEX0;
+                      const int i1_offset = i1+FACEX1;
+                      const int i2_offset = i2+FACEX2;
+
+                      // Initialize derivatives to crazy values, to ensure that
+                      //   we will notice in case they aren't set properly.
+                      REAL r_offset = 1e100;
+                      REAL partial_i_f_offset = 1e100;
+
+                      contraction_term(params, which_gf, gfs, xx, FACEXi, i0_offset, i1_offset, i2_offset, &r_offset, &partial_i_f_offset);
+
+                      const REAL invr_offset = 1./r_offset;
+
+                      // Pure advection: [FIXME: Add equation (appearing in Jupyter notebook documentation)]
+                      const REAL extrap_rhs = char_speed*(partial_i_f_offset + invr_offset*(gfs[IDX4S(which_gf,i0_offset,i1_offset,i2_offset)] - var_at_infinity));
+
+                      // Take difference between pure advection and df/dt|interior
+                      const REAL diff_between_advection_and_f_rhs =
+                          rhs_gfs[IDX4S(which_gf,i0_offset,i1_offset,i2_offset)] + extrap_rhs;
+
+                      // Solve for k/(r_gz)^n+1 term
+                      rhs_gfs[IDX4S(which_gf,i0,i1,i2)] += diff_between_advection_and_f_rhs*pow(r_offset*invr,radial_falloff_power);
+
+                  }
+                } // END for(int pt=0;pt<num_ob_gz_pts[which_gz];pt++)
+
+            // Apply INNER (parity) boundary conditions:
+                for(int pt=0;pt<bcstruct->num_ib_gz_pts[which_gz];pt++) {
+                    const int i0dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i0;
+                    const int i1dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i1;
+                    const int i2dest = bcstruct->inner[which_gz][pt].inner_bc_dest_pt.i2;
+                    const int i0src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i0;
+                    const int i1src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i1;
+                    const int i2src  = bcstruct->inner[which_gz][pt].inner_bc_src_pt.i2;
+
+                    rhs_gfs[IDX4S(which_gf,i0dest,i1dest,i2dest)] =
+                            bcstruct->inner[which_gz][pt].parity[gfs_parity[which_gf]] * rhs_gfs[IDX4S(which_gf, i0src,i1src,i2src)];
+            } // END for(int pt=0;pt<num_ib_gz_pts[which_gz];pt++)
+        } // END for(int which_gz = 0; which_gz < NGHOSTS; which_gz++)
+    } // END for(int which_gf=0;which_gf<NUM_GFS;which_gf++)
+} // END function
+"""
+        with open(os.path.join(Ccodesdir,"boundary_conditions/apply_bcs_sommerfeld.h"),"w") as file:
+            file.write(main_Ccode)
+
+    def write_sommerfeld_file(self, Ccodesdir):
+        self.write_sommerfeld_main_Ccode(Ccodesdir)
+        print("""\nSuccessfully generated Sommerfeld boundary condition C code""")
