ea1559ea4b3be593b2ee74a4ce37c8ce4aa97e7a
Terrence Pierre <terrencepierrej@gmail.com>
2021-01-25 16:02:37 -0500

    Updated Maxwell Unit tests

--- Maxwell/tests/test_Maxwell.py
+++ Maxwell/tests/test_Maxwell.py
@@ -1,52 +1,68 @@
 from UnitTesting.create_test import create_test
 
 
-def test_MaxwellCartesian_Evol_System_I():
+def test_VacuumMaxwell_Flat_Evol_Cartesian_System_I():
 
-    module = 'Maxwell.MaxwellCartesian_Evol'
+    module = 'Maxwell.VacuumMaxwell_Flat_Evol_Cartesian'
 
-    module_name = 'MaxwellCartesian_Evol_System_I'
+    module_name = 'VacuumMaxwell_Flat_Evol_Cartesian_System_I'
 
-    function_and_global_dict = {'MaxwellCartesian_Evol()': ['ArhsD', 'ErhsD', 'psi_rhs', 'Cviolation']}
+    function_and_global_dict = {'VacuumMaxwellRHSs()': ['ArhsU', 'ErhsU', 'C', 'psi_rhs']}
 
-    initialization_string_dict = {'MaxwellCartesian_Evol()': 'import NRPy_param_funcs as par\npar.set_parval_from_str("System_to_use","System_I")'}
-
-    create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict)
-
-def test_MaxwellCartesian_Evol_System_II():
-
-    module = 'Maxwell.MaxwellCartesian_Evol'
-
-    module_name = 'MaxwellCartesian_Evol_System_II'
-
-    function_and_global_dict = {'MaxwellCartesian_Evol()': ['ArhsD', 'ErhsD', 'psi_rhs', 'Gamma_rhs', 'Cviolation']}
-
-    create_test(module, module_name, function_and_global_dict)
-
-
-def test_MaxwellCartesian_ID_System_I():
-
-    module = 'Maxwell.MaxwellCartesian_ID'
-
-    module_name = 'MaxwellCartesian_ID_System_I'
-
-    function_and_global_dict = {'MaxwellCartesian_ID()': ['AidD', 'EidD', 'psi_ID']}
-
-    initialization_string_dict = {'MaxwellCartesian_ID()': 'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", __name__, "System_to_use", "System_I"))'}
+    initialization_string_dict = {'VacuumMaxwellRHSs()': 'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", "Maxwell.InitialData","System_to_use","System_I"))'}
 
     create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict)
 
-def test_MaxwellCartesian_ID_System_II():
+def test_VacuumMaxwell_Flat_Evol_Cartesian_System_II():
 
-    module = 'Maxwell.MaxwellCartesian_ID'
+    module = 'Maxwell.VacuumMaxwell_Flat_Evol_Cartesian'
 
-    module_name = 'MaxwellCartesian_ID_System_II'
+    module_name = 'VacuumMaxwell_Flat_Evol_Cartesian_System_II'
 
-    function_and_global_dict = {'MaxwellCartesian_ID()': ['AidD', 'EidD', 'psi_ID']}
+    function_and_global_dict = {'VacuumMaxwellRHSs()': ['ArhsU', 'ErhsU', 'C', 'psi_rhs', 'Gamma_rhs', 'G']}
 
-    initialization_string_dict = {'MaxwellCartesian_ID()': 'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", __name__, "System_to_use", "System_II"))'}
+    initialization_string_dict = {'VacuumMaxwellRHSs()': 'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", "Maxwell.InitialData","System_to_use","System_II"))'}
 
     create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict)
+
+
+def test_VacuumMaxwell_Flat_Evol_Curvilinear_rescaled():
+
+    module = 'Maxwell.VacuumMaxwell_Flat_Evol_Curvilinear_rescaled'
+
+    module_name = 'VacuumMaxwell_Flat_Evol_Curvilinear_rescaled'
+
+    function_and_global_dict = {'VacuumMaxwellRHSs_rescaled()': ['erhsU', 'arhsU', 'psi_rhs', 'Gamma_rhs', 'C', 'G', 'EU_Cart', 'AU_Cart']}
+
+    initialization_string_dict = {'VacuumMaxwellRHSs_rescaled()':  'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", "Maxwell.InitialData","System_to_use","System_II"))'}
+
+    create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict)
+
+
+
+def test_InitialData_I():
+
+    module = 'Maxwell.InitialData'
+
+    module_name = 'InitialData_System_I'
+
+    function_and_global_dict = {'InitialData()': ['AidU', 'EidU', 'psi_ID']}
+
+    initialization_string_dict = {'InitialData()': 'import NRPy_param_funcs as par\npar.initialize_param(par.glb_param("char", "Maxwell.InitialData","System_to_use","System_I"))'}
+
+    create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict)
+
+def test_InitialData_II():
+
+    module = 'Maxwell.InitialData'
+
+    module_name = 'InitialData_System_II'
+
+    function_and_global_dict = {'InitialData()': ['AidU', 'EidU', 'psi_ID', 'Gamma_ID']} # issue here
+
+    initialization_string_dict = {'InitialData()': 'import NRPy_param_funcs as par\npar.set_parval_from_str("Maxwell.InitialData::System_to_use","System_II")'}
+
+    create_test(module, module_name, function_and_global_dict, initialization_string_dict=initialization_string_dict, logging_level='DEBUG')
 
 
 if __name__ == '__main__':
